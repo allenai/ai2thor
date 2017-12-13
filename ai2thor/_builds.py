1bd41ebc00ffbed3e0acd840dce3cf23c2eb923c
Eric Kolve <erick@allenai.org>
2017-12-13 13:02:50 -0800

    auto-generating _builds.py after sucessful build with urls/sha256

---
+++ ai2thor/_builds.py
@@ -0,0 +1,5 @@
+# GENERATED FILE - DO NOT EDIT
+BUILDS = {'Darwin': {'sha256': 'aaae2303bd0f707fa4620f5d4e36e05362449e4a4a9972c4a0612185a873b9f9',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712131251-OSXIntel64.zip'},
+ 'Linux': {'sha256': '82a95533aa368f037f4c41970de7ae576637dac23f324815e2c4ec47558c6403',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712131251-Linux64.zip'}}
