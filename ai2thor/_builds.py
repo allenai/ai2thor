8a279cf504af5fad0a5aef758701e4e218410b50
Eric Kolve <erick@allenai.org>
2017-12-14 13:14:22 -0800

    upgrading to 5.4.6

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,5 +1,5 @@
 # GENERATED FILE - DO NOT EDIT
-BUILDS = {'Darwin': {'sha256': 'aaae2303bd0f707fa4620f5d4e36e05362449e4a4a9972c4a0612185a873b9f9',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712131251-OSXIntel64.zip'},
- 'Linux': {'sha256': '82a95533aa368f037f4c41970de7ae576637dac23f324815e2c4ec47558c6403',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712131251-Linux64.zip'}}
+BUILDS = {'Darwin': {'sha256': '609a2990cec2a9e292abc609d138b9f9b50761d1ef403e92dd0dfedd6fb24700',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712141159-OSXIntel64.zip'},
+ 'Linux': {'sha256': '2ee629c37e3ef0efcfa68c217ac913e90903057952705e8b308032e8b2e49e7d',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712141159-Linux64.zip'}}
