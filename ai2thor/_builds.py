0e2bcd56bf78db269cdef9d87e09fd9eecf0c42c
Eric Kolve <erick@allenai.org>
2017-12-20 14:08:08 -0800

    pushing builds for FP7 and FP20 fixes

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,5 +1,5 @@
 # GENERATED FILE - DO NOT EDIT
-BUILDS = {'Darwin': {'sha256': '3a669393537868428f3187bc4d8dc3adb75e887277f8aa83883d02a5e2a80962',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201338-OSXIntel64.zip'},
- 'Linux': {'sha256': 'bf39c26655a4c62da0133b1d53febc28cee3d1c02d3ea23a4e2aef67d7ba73eb',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201338-Linux64.zip'}}
+BUILDS = {'Darwin': {'sha256': '91fcd1b260cc616d54ea2f775546e5de2560d82e638cbf169e2a978d36041521',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201400-OSXIntel64.zip'},
+ 'Linux': {'sha256': 'edecaab3e20f2ed2d779a685cc032ec34479216f743d172ebd39a8e2d0d9d9d1',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201400-Linux64.zip'}}
