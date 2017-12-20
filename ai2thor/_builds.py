7f9334a515cf1539f45043df7ccd2b40ef168e1f
Eric Kolve <erick@allenai.org>
2017-12-20 13:45:04 -0800

    upgrading to include improved Move logic for bfs searching

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,5 +1,5 @@
 # GENERATED FILE - DO NOT EDIT
-BUILDS = {'Darwin': {'sha256': 'b1efd966b47aeec100d197fa718981ff660e6f72ce5b4b06fd718258e4e3cbfd',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201205-OSXIntel64.zip'},
- 'Linux': {'sha256': '2a48478762d15ea2dc5345639448142f357dcafb98f898c9fee33b8798c992ed',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201205-Linux64.zip'}}
+BUILDS = {'Darwin': {'sha256': '3a669393537868428f3187bc4d8dc3adb75e887277f8aa83883d02a5e2a80962',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201338-OSXIntel64.zip'},
+ 'Linux': {'sha256': 'bf39c26655a4c62da0133b1d53febc28cee3d1c02d3ea23a4e2aef67d7ba73eb',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201712201338-Linux64.zip'}}
