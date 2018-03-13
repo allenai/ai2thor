b86a976e12af91702391f0ab4634812aac6b3834
Winson Han <winson.whan@gmail.com>
2018-03-13 09:30:15 -0700

    Revert "Revert "revving to 0.0.17""
    This reverts commit a729f20f3428f6e02c4952fed2f055e2ead24b3d.

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,7 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201802061507'
-BUILDS = {'Darwin': {'sha256': '49b4fd6f273af57485e3510b6f32190b1b8915aae59336625b36219863d2eb90',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201802061507-OSXIntel64.zip'},
- 'Docker': {'tag': '201802061507'},
- 'Linux': {'sha256': '9fa2a1a7a6e5776601c37706a96be3eb72c614f87b9625dcba2ee5dbd7184ac7',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201802061507-Linux64.zip'}}
+VERSION = '201802281026'
+BUILDS = {'Darwin': {'sha256': 'ed1f82afeef43915f2c95569fab0d52fe85b261ac54f21cc45145f6bc4ab5c89',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201802281026-OSXIntel64.zip'},
+ 'Docker': {'tag': '201802281026'},
+ 'Linux': {'sha256': '8d1b4b772f872edc57db5faef94701b156aabdda68950fdabb63f069bf357140',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201802281026-Linux64.zip'}}
