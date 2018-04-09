046fb0b8c0ac71a2937c94827cad9b5693a61cf3
Winson Han <winson.whan@gmail.com>
2018-04-09 11:28:50 -0700

    FP7 Drawer Fix

--- unity/Assets/Scripts/Cabinet.cs
+++ unity/Assets/Scripts/Cabinet.cs
@@ -2,7 +2,8 @@
 using UnityEngine;
 using System.Collections;
 
-public enum CabinetOpenStyle {
+public enum CabinetOpenStyle 
+{
 	SingleDoorLeft,
 	SingleDoorRight,
 	DoubleDoors,
@@ -10,16 +11,20 @@ public enum CabinetOpenStyle {
 }
 
 [ExecuteInEditMode]
-public class Cabinet : MonoBehaviour {
+public class Cabinet : MonoBehaviour 
+{
 
 	public bool Animate = false;
 	public bool Open;
 	public SimObj ParentObj;
 
+    public Transform VisCollider; //the visibility collider for reference if this is a drawer
+
 	public CabinetOpenStyle OpenStyle;
 	public Transform LeftDoor;
 	public Transform RightDoor;
-	public Transform DrawerDoor;
+	public Transform DrawerDoor; //DRAWER
+
 
 	public Vector3 OpenAngleRight = new Vector3 (0f, -90f, 0f);
 	public Vector3 OpenAngleLeft = new Vector3 (0f, 90f, 0f);
@@ -28,9 +33,22 @@ public class Cabinet : MonoBehaviour {
 	public Vector3 ClosedLocalPosition = new Vector3 (0f, 0f, 0f);
 	public Vector3 OpenLocalPosition = new Vector3(0f, 0f, 0f);
 
+
 	Vector3 rightDoorTargetRotation;
 	Vector3 leftDoorTargetRotation;
-	Vector3 drawerTargetPosition;
+
+    //Drawer 
+	Vector3 drawerTargetPosition; //Drawer's position if either open or closed
+
+    Vector3 drawerVisColPosition; //Drawer's visibility collider position if open or closed
+    Vector3 drawerVisColScale;    //Drawer's visibility collider scale if open or closed
+
+    public Vector3 ClosedVisColPosition = new Vector3(0f, 0f, 0f); //Drawer's visibility collider position if CLOSED
+    public Vector3 ClosedVisColScale = new Vector3(1f, 1f, 0.2f); //drawer's visiblity collider scale if CLOSED
+
+    public Vector3 OpenVisColPosition = new Vector3(0f, 0f, -0.17f); //drawer's visibility collider position if OPEN
+    public Vector3 OpenVisColScale = new Vector3(1f, 1f, 1f); //drawer's visiiblity collider scale if OPEN
+
 	//bool lastOpen = false;
 	float animatingDistance;
 
@@ -119,6 +137,10 @@ public class Cabinet : MonoBehaviour {
 			drawerTargetPosition = open ? OpenLocalPosition : ClosedLocalPosition;
 			DrawerDoor.localPosition = Vector3.Lerp (DrawerDoor.localPosition, drawerTargetPosition, Time.deltaTime * SimUtil.SmoothAnimationSpeed);
 			animatingDistance = Vector3.Distance (DrawerDoor.localPosition, drawerTargetPosition);
+
+            //drawerVisColScale = open ? OpenVisColScale : ClosedVisColScale;
+            //drawerVisColPosition = open ? OpenVisColPosition : ClosedVisColPosition;
+            
 			break;
 		}
 
@@ -128,8 +150,10 @@ public class Cabinet : MonoBehaviour {
 		ParentObj.IsAnimating = (animatingDistance > 0.0025f);
 	}
 
-	void UpdateAnimationInstant(bool open) {
-		switch (OpenStyle) {
+	void UpdateAnimationInstant(bool open) 
+    {
+		switch (OpenStyle) 
+        {
 		case CabinetOpenStyle.DoubleDoors:
 			RightDoor.transform.localEulerAngles = open ? OpenAngleRight : ClosedAngleRight;
 			LeftDoor.transform.localEulerAngles = open ? OpenAngleLeft : ClosedAngleLeft;
@@ -145,6 +169,11 @@ public class Cabinet : MonoBehaviour {
 
 		case CabinetOpenStyle.Drawer:
 			DrawerDoor.transform.localPosition = open ? OpenLocalPosition : ClosedLocalPosition;
+            
+            //if the drawer is closed, move the vis collider to the outer edge so it is visible
+            //if the door is open, move visibility collider so agent can pick up/place things in drawer
+            VisCollider.transform.localPosition = open ? OpenVisColPosition : ClosedVisColPosition;
+            VisCollider.transform.localScale = open ? OpenVisColScale : ClosedVisColScale;
 			break;
 		}
 	}
