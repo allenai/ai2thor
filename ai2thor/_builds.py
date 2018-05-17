ab021110362d528eb297f61d02138b84d3991ec6
Eric Kolve <erick@allenai.org>
2018-05-17 13:59:28 -0700

    v0.0.28 - Multi-agent support; adding visibilityDistance as Initialize parameter

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,7 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201805111040'
-BUILDS = {'Darwin': {'sha256': '545c733ad8989ceece6cb38f6b0c69f7c54934271bcf829e6da90b4fdeb23063',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805111040-OSXIntel64.zip'},
- 'Docker': {'tag': '201805111040'},
- 'Linux': {'sha256': '25947d71705098aa0f5bd6f3a52ee3a7b70831bf631a463304661e0db9693aef',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805111040-Linux64.zip'}}
+VERSION = '201805171333'
+BUILDS = {'Darwin': {'sha256': 'bbb75a8241242b67a4a4d4d9f05f5635cdd4ab7a1a15d47f358513fc59f4fea8',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805171333-OSXIntel64.zip'},
+ 'Docker': {'tag': '201805171333'},
+ 'Linux': {'sha256': '03f43d8febc43155fb8327086b04ef5a3e309b210e7c9aa6659c49f3366039b1',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805171333-Linux64.zip'}}
