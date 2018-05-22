2840d01f51f601832e5d74b505b24b5935ac74b7
Eric Kolve <erick@allenai.org>
2018-05-22 10:45:40 -0700

    Final 0.0.28 build - adding limit of 6 to number of multi-agents

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,7 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201805171333'
-BUILDS = {'Darwin': {'sha256': 'bbb75a8241242b67a4a4d4d9f05f5635cdd4ab7a1a15d47f358513fc59f4fea8',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805171333-OSXIntel64.zip'},
- 'Docker': {'tag': '201805171333'},
- 'Linux': {'sha256': '03f43d8febc43155fb8327086b04ef5a3e309b210e7c9aa6659c49f3366039b1',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805171333-Linux64.zip'}}
+VERSION = '201805221013'
+BUILDS = {'Darwin': {'sha256': '3c8124e109ea8d84d99c0b286a566d44e13152df53e53e84ef6c8ad1ebef0ac8',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805221013-OSXIntel64.zip'},
+ 'Docker': {'tag': '201805221013'},
+ 'Linux': {'sha256': '3ea81482e273206be6a615ab3a6dd208ffe65ffa7cf85441a3a5e4e0ad3747d6',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805221013-Linux64.zip'}}
