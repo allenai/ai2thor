e7bbbf8428e5dbfddea9f90084e53ec7ce56bbb0
Winson Han <winson.whan@gmail.com>
2018-05-24 12:41:06 -0700

    text input functions in

--- unity/Assets/DebugInputField.cs
+++ unity/Assets/DebugInputField.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugInputField : MonoBehaviour 
+namespace UnityStandardAssets.Characters.FirstPerson
 {
+	public class DebugInputField : MonoBehaviour
+    {
+		public GameObject Agent = null;
 
-	// Use this for initialization
-	void Start () 
-	{
-		//UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(gameObject);
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+            //UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(gameObject);
+        }
 
-    public void Execute(string command)
-	{
-		print(command);
+        // Update is called once per frame
+        void Update()
+        {
 
-	}
+        }
+
+        public void Execute(string command)
+        {
+            //print(command);
+            if (command == "moveforward")
+            {
+				//MoveForward();
+				Agent.GetComponent<DebugFPSAgentController>().MoveForward();
+            }
+        }
+    }
 }
+
