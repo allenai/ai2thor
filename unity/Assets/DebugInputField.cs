fa08ddf3b15994061e405446c220cf74bb910487
Winson Han <winson.whan@gmail.com>
2018-05-25 17:16:32 -0700

    move/rotate/look functions via text input debug controller

--- unity/Assets/DebugInputField.cs
+++ unity/Assets/DebugInputField.cs
@@ -7,11 +7,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
 	public class DebugInputField : MonoBehaviour
     {
 		public GameObject Agent = null;
+		public DebugFPSAgentController AgentController = null;
 
         // Use this for initialization
         void Start()
         {
-            //UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(gameObject);
+			//UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(gameObject);
+			AgentController = Agent.GetComponent<DebugFPSAgentController>();
         }
 
         // Update is called once per frame
@@ -22,12 +24,66 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         public void Execute(string command)
         {
-            //print(command);
+            ///////////////////
+			/// move forward, back, left, right
             if (command == "moveforward")
             {
 				//MoveForward();
-				Agent.GetComponent<DebugFPSAgentController>().MoveForward();
+				AgentController.MoveAgent("forward", 0.5f);
             }
+
+            if (command == "moveleft")
+			{
+				AgentController.MoveAgent("left", 0.5f);
+			}
+
+			if (command == "moveright")
+			{
+				AgentController.MoveAgent("right", 0.5f);
+			}
+
+            if (command == "movebackward")
+			{
+				AgentController.MoveAgent("backward", 0.5f);
+			}
+
+            //////////////////////
+			/// camera rotate stuff
+            if(command =="lookup")
+			{
+				AgentController.Look("up");
+    		}
+
+			if (command == "lookforward")
+            {
+				AgentController.Look("forward");
+            }
+
+			if (command == "lookdown")
+            {
+				AgentController.Look("down");
+            }
+
+            if( command == "looksuperdown")
+			{
+				AgentController.Look("superdown");
+
+			}
+
+            /////////////////////
+			/// rotate
+            if (command == "turnleft")
+			{
+				AgentController.Turn("left");
+			}
+
+			if (command == "turnright")
+            {
+				AgentController.Turn("right");
+
+            }
+
+           
         }
     }
 }
