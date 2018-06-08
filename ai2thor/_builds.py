606bd0177ffae3807092def3abc41735ee027528
Eric Kolve <erick@allenai.org>
2018-06-07 17:22:24 -0700

    Revving version 0.0.30 - adding IQA dependencies: depth, segmentation, masks

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,7 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201805221013'
-BUILDS = {'Darwin': {'sha256': '3c8124e109ea8d84d99c0b286a566d44e13152df53e53e84ef6c8ad1ebef0ac8',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805221013-OSXIntel64.zip'},
- 'Docker': {'tag': '201805221013'},
- 'Linux': {'sha256': '3ea81482e273206be6a615ab3a6dd208ffe65ffa7cf85441a3a5e4e0ad3747d6',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201805221013-Linux64.zip'}}
+VERSION = '201806071709'
+BUILDS = {'Darwin': {'sha256': 'd5b5901ed4426164d58c469f60130cf6bff2a51b76b1ca9afba3e05304a7e959',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201806071709-OSXIntel64.zip'},
+ 'Docker': {'tag': '201806071709'},
+ 'Linux': {'sha256': 'b6db7f9ec81220acc27327b195f625f11264c4000ef0bae51c912d3173c798d8',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201806071709-Linux64.zip'}}
