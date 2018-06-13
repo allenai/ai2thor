0418e3e0daf21809cc4018a2885d2d026a7cc321
Eric Kolve <erick@allenai.org>
2018-06-13 13:51:28 -0700

    flake8 fixes for downloader.py

--- ai2thor/downloader.py
+++ ai2thor/downloader.py
@@ -11,8 +11,11 @@ def download(url, build_name, sha256_digest):
     r = requests.get(url, stream=True)
     size = int(r.headers['Content-Length'].strip())
     total_bytes = 0
-    widgets = [build_name, ": ", Bar(marker="|", left="[", right=" "),
-        Percentage(), " ",  FileTransferSpeed(), "]  of {0}MB".format(str(round(size / 1024 / 1024, 2))[:4])]
+
+    widgets = [
+        build_name, ": ", Bar(marker="|", left="[", right=" "),
+        Percentage(), " ", FileTransferSpeed(), "]  of {0}MB".format(str(round(size / 1024 / 1024, 2))[:4])]
+
     pbar = ProgressBar(widgets=widgets, maxval=size).start()
     m = hashlib.sha256()
     file_data = []
