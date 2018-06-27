b5a361ca9b524fee1f9287272ad17d92ef7075de
Eli VanderBilt <eliv@allenai.org>
2018-06-27 14:36:27 -0700

    Added apples/apple slices to physics assets

---
+++ unity/Assets/Editor/CreatePrefabFromSelected.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class CreatePrefabFromSelected
+{
+	const string menuTitle = "GameObject/Create Prefab From Selected";
+
+	/// <summary>
+	/// Creates a prefab from the selected game object.
+	/// </summary>
+	[MenuItem (menuTitle)]
+	static void CreatePrefab ()
+	{
+		GameObject obj = Selection.activeGameObject;
+		string name = obj.name;
+
+		Object prefab = EditorUtility.CreateEmptyPrefab("Assets/" + name + ".prefab");
+		EditorUtility.ReplacePrefab(obj, prefab);
+		AssetDatabase.Refresh();
+	}
+
+	/// <summary>
+	/// Validates the menu.
+	/// </summary>
+	/// <remarks>The item will be disabled if no game object is selected.</remarks>
+	[MenuItem (menuTitle, true)]
+	static bool ValidateCreatePrefab ()
+	{
+		return Selection.activeGameObject != null;
+	}
+}
