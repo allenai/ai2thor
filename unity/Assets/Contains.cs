83eb178c8acb2a39d513de2ccd2f135debe3aaa6
Winson Han <winson.whan@gmail.com>
2018-07-05 16:17:42 -0700

    FloorPlan1_physics ready for testing
    Sample kitchen Sim Objects have been placed in this kitchen scene. Ready for object manipulation/sight functionality testing

--- unity/Assets/Contains.cs
+++ unity/Assets/Contains.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Contains : MonoBehaviour 
 {
 	//used to not add the door/drawer/etc of the object itself to the list of currently contained objects
-    [SerializeField] protected List<GameObject> MyDoorsOrDrawer = null; 
+	[SerializeField] protected List<GameObject> MyObjects = null; //use this for any colliders that should be ignored (each cabinet should ignore it's own door, etc)
 
 	[SerializeField] protected List<SimObjPhysics> CurrentlyContains = new List<SimObjPhysics>();
    
@@ -34,7 +34,7 @@ public class Contains : MonoBehaviour
 			SimObjPhysics sop = other.GetComponentInParent<SimObjPhysics>();
 
             //check each "other" object, see if it is currently in the CurrentlyContains list, and make sure it is NOT one of this object's doors/drawer
-			if (!CurrentlyContains.Contains(sop) && !MyDoorsOrDrawer.Contains(sop.transform.gameObject))
+			if (!CurrentlyContains.Contains(sop) && !MyObjects.Contains(sop.transform.gameObject))
             {
 				CurrentlyContains.Add(sop);
             }         
