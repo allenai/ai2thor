f42f400b70c6539e7a68b0f45d0ee3c0a46177c9
Winson Han <winson.whan@gmail.com>
2018-08-14 12:24:00 -0700

    added MassScale assets from Hackathon
    all added into the Physics->MassComparison folder to keep things organized

---
+++ unity/Assets/Physics/MassComparison/Adoption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//because it's not full parenting, eh? eh? it's a pun!
+
+public class Adoption : MonoBehaviour 
+{
+	public GameObject adoptedParent;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		gameObject.transform.position = adoptedParent.transform.position;
+	}
+}
