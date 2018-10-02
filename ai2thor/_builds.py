279c4f43ea34694ed6f157f185765ce239b87410
Eric Kolve <erick@allenai.org>
2018-10-02 13:07:02 -0700

    revving to 0.0.44

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,7 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201809121509'
-BUILDS = {'Darwin': {'sha256': 'e922af671d7baccc099a8bf1e57f40b32d4e92b2abd144437c05da0ce5961abd',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201809121509-OSXIntel64.zip'},
- 'Docker': {'tag': '201809121509'},
- 'Linux': {'sha256': '8dc61edce77e3959235bb74230aa51fef7877120ff68ef646e800cc791f36909',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201809121509-Linux64.zip'}}
+VERSION = '201810021256'
+BUILDS = {'Darwin': {'sha256': '57fa82a66ae560666314d7fdb99c6681f455da3822f691fd97c5dfe952dcf2bf',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201810021256-OSXIntel64.zip'},
+ 'Docker': {'tag': '201810021256'},
+ 'Linux': {'sha256': '3811b80a1dff6492d9a3c2c59e655a7f1361d92b1ca12c7e4daae37ca8fb3498',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201810021256-Linux64.zip'}}
