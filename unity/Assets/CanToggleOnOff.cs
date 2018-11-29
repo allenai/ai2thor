73ca70b507a3fcf1a42b84211c0db2fa67b37d3a
Winson Han <winson.whan@gmail.com>
2018-11-29 13:10:59 -0800

    FP24-26 Lightswitches added. Cleaned up CanToggleOnOff.cs

--- unity/Assets/CanToggleOnOff.cs
+++ unity/Assets/CanToggleOnOff.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//serialized class so it can show up in Inspector Window
 [System.Serializable]
 public class SwapObjList
 {
+	//reference to game object that needs to have materials changed
 	[Header("Object That Needs Mat Swaps")]
 	[SerializeField]
 	public GameObject MyObject;
 
+	//copy the Materials array on MyObject's Renderer component here
 	[Header("Materials for On state")]
 	[SerializeField]
 	public Material[] OnMaterials;
 
-	//swap to this array of materials when off
+	//swap to this array of materials when off, usually just one or two materials will change
 	[Header("Materials for Off state")]
 	[SerializeField]
 	public Material[] OffMaterials;
@@ -195,10 +198,9 @@ public class CanToggleOnOff : MonoBehaviour
 				}
 			}
 		}
-
-		
 	}
 
+	//toggle isOn variable, swap Materials and enable/disable Light sources
 	private void setisOn()
 	{
 		//if isOn true, set it to false and also turn off all lights/deactivate materials
