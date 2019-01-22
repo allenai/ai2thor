efcd55459bab9ac8d933bdf50267475fe69f7bd8
Winson Han <winson.whan@gmail.com>
2019-01-22 10:52:08 -0800

    added ObjectSpecificReceptacle logic...
    specifically for receptacles that can only hold a specifid object, ie: Toilet Paper Hangers only hold toilet paper and rolls, Towel Holders only hold Towels, Coffee Makers only hold cups/mugs - These must follow a different logic for placement compared to normal receptacles which use a "spawn zone" based logic rather than a "specific attachment point" logic

---
+++ unity/Assets/ObjectSpecificReceptacle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpecificReceptacle : MonoBehaviour 
+{
+	[Header("Only objects of these Types can be placed on this Receptacle")]
+	public SimObjType[] SpecificTypes;
+
+	[Header("Point where specified object(s) attach to this Receptacle")]
+	public Transform attachPoint;
+
+	[Header("Is this Receptacle already holding a valid object?")]
+	public bool full = false;
+
+	public bool HasSpecificType(SimObjType check)
+	{
+		bool result = false;
+
+		foreach(SimObjType sot in SpecificTypes)
+		{
+			if(sot == check)
+			{
+				result = true;
+			}
+		}
+
+		return result;
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		#if UNITY_EDITOR
+		if(!gameObject.GetComponent<SimObjPhysics>().DoesThisObjectHaveThisSecondaryProperty(SimObjSecondaryProperty.ObjectSpecificReceptacle))
+		{
+			Debug.LogError(this.name + " is missing the Secondary Property ObjectSpecificReceptacle!");
+		}
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
