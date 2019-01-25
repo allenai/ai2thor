39d01babcb7c908c3f0566a97a2d2779becc5ccc
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-01-25 11:30:04 -0800

    Added CreatePrefab Script

---
+++ unity/Assets/Scripts/CreatePrefab.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreatePrefab : MonoBehaviour
+{
+
+	[MenuItem("Extras/Create Prefab From Selection")]
+	static void DoCreatePrefab()
+	{
+		Transform[] transforms = Selection.transforms;
+		foreach (Transform t in transforms)
+		{
+			GameObject prefab = PrefabUtility.CreatePrefab("Assets/Prefabs/" + t.gameObject.name + ".prefab", t.gameObject, ReplacePrefabOptions.ReplaceNameBased);
+		}
+	}
+}
