cd3bfb55bdccb1e2852caac8e7aba8615f222cd5
Winson Han <winson.whan@gmail.com>
2019-01-28 17:19:45 -0800

    Drone movement prototype 1.0
    ...got basic movement and basic functions of drone and object launcher prototyped

---
+++ unity/Assets/Scripts/DroneBasket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+public class DroneBasket : MonoBehaviour 
+{
+	public GameObject myParent = null;
+
+	[SerializeField] protected List<SimObjPhysics> CurrentlyContains = new List<SimObjPhysics>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		//from the collider, see if the thing hit is a sim object physics
+		//don't detect other trigger colliders to prevent nested objects from containing each other
+		if (other.GetComponentInParent<SimObjPhysics>() && !other.isTrigger)
+		{
+			
+			SimObjPhysics sop = other.GetComponentInParent<SimObjPhysics>();
+
+			//don't add any parent objects in case this is a child sim object
+			if(sop.transform == myParent.transform)
+			{
+				return;
+			}
+
+			//check each "other" object, see if it is currently in the CurrentlyContains list, and make sure it is NOT one of this object's doors/drawer
+			if (!CurrentlyContains.Contains(sop))//&& !MyObjects.Contains(sop.transform.gameObject))
+			{
+				CurrentlyContains.Add(sop);
+
+				sop.GetComponent<Transform>().SetParent(this.transform);
+				sop.transform.Find("Colliders").gameObject.SetActive(false);
+				sop.transform.Find("TriggerColliders").gameObject.SetActive(false);
+				sop.transform.Find("BoundingBox").gameObject.SetActive(false);
+
+				if(sop.GetComponent<Rigidbody>())
+				Destroy(sop.GetComponent<Rigidbody>());
+
+				sop.enabled = false;
+
+				myParent.GetComponent<FlyingDrone>().caught = true;
+			}
+		}
+	}
+
+}
---
+++ unity/Assets/Scripts/FlyingDrone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingDrone : MonoBehaviour 
+{
+
+	[SerializeField] GameObject basket;
+	[SerializeField] GameObject basketTrigger;
+
+	[SerializeField] DroneObjectLauncher DroneObjectLauncher;
+	
+	public bool caught = false;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public bool isCaught()
+	{
+		return caught;
+	}
+
+	public void Launch(ServerAction action)
+	{
+		DroneObjectLauncher.Launch(action.moveMagnitude, action.rotation);
+	}
+}
