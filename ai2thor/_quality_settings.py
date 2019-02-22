26039beb70486f25afffdd90a3e0a896c4568765
AlvaroHG <alvaroh@MacBook-Pro-4.local>
2019-02-22 14:36:02 -0800

    Quality settings change

--- ai2thor/_quality_settings.py
+++ ai2thor/_quality_settings.py
@@ -1,10 +1,10 @@
 # GENERATED FILE - DO NOT EDIT
 DEFAULT_QUALITY = 'Ultra'
 QUALITY_SETTINGS = {'High': 4,
- 'High WebGL': 5,
+ 'High WebGL': 7,
  'Low': 1,
  'Medium': 2,
  'MediumCloseFitShadows': 3,
- 'Ultra': 7,
- 'Very High': 6,
+ 'Ultra': 6,
+ 'Very High': 5,
  'Very Low': 0}
