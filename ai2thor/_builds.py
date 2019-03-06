96d8e5852c8b98d6135dde7cce0da2b5590b4687
Eric Kolve <erick@allenai.org>
2019-03-06 13:15:58 -0800

    adding build-class-dataset task to generate image crops by class

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,7 +1,3 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201808161213'
-BUILDS = {'Darwin': {'sha256': '1fe96a9d1df19749e7c86879743979b538eadd1502457b7c945cbfdc5283942b',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201808161213-OSXIntel64.zip'},
- 'Docker': {'tag': '201808161213'},
- 'Linux': {'sha256': 'e797ff79078aaaae2d0244c2d457af13e30b6c27e56369162d05da1f0574f75f',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201808161213-Linux64.zip'}}
+VERSION = ''
+BUILDS = {}
--- tasks.py
+++ tasks.py
@@ -1,4 +1,3 @@
-import pprint
 import os
 import datetime
 import zipfile
@@ -6,6 +5,7 @@ import threading
 import hashlib
 import shutil
 import subprocess
+import pprint
 from invoke import task
 
 S3_BUCKET = 'ai2-thor'
@@ -60,6 +60,151 @@ def _build(unity_path, arch, build_dir, build_name, env={}):
     full_env.update(env)
     full_env['UNITY_BUILD_NAME'] = target_path
     subprocess.check_call(command, shell=True, env=full_env)
+
+def class_dataset_images_for_scene(scene_name):
+    import ai2thor.controller
+    from itertools import product
+    from collections import defaultdict
+    import numpy as np
+    import cv2
+    import hashlib
+    import json
+
+    env = ai2thor.controller.Controller(quality='Low')
+    player_size = 300
+    zoom_size = 1000
+    target_size = 256
+    rotations = [0, 90, 180, 270]
+    horizons = [330,  0, 30]
+    buffer = 15
+    # object must be at least 40% in view
+    min_size = ((target_size * 0.4)/zoom_size) * player_size
+
+    env.start(player_screen_width=player_size, player_screen_height=player_size)
+    env.reset(scene_name)
+    event = env.step(dict(action='Initialize', gridSize=0.25, renderObjectImage=True, renderClassImage=False, renderImage=False))
+
+    for o in event.metadata['objects']:
+        if o['receptacle'] and o['receptacleObjectIds'] and o['openable']:
+            print("opening %s" % o['objectId'])
+            env.step(dict(action='OpenObject', objectId=o['objectId'], forceAction=True))
+
+    event = env.step(dict(action='GetReachablePositions', gridSize=0.25))
+    
+    visible_object_locations = []
+    for point in event.metadata['actionReturn']:
+        for rot, hor in product(rotations, horizons):
+            exclude_colors = set(map(tuple, np.unique(event.instance_segmentation_frame[0], axis=0)))
+            exclude_colors.update(set(map(tuple, np.unique(event.instance_segmentation_frame[:, -1, :], axis=0))))
+            exclude_colors.update(set(map(tuple, np.unique(event.instance_segmentation_frame[-1], axis=0))))
+            exclude_colors.update(set(map(tuple, np.unique(event.instance_segmentation_frame[:, 0, :], axis=0))))
+
+            event = env.step(dict( action='TeleportFull', x=point['x'], y=point['y'], z=point['z'], rotation=rot, horizon=hor, forceAction=True), raise_for_failure=True) 
+
+            visible_objects = []
+
+            for o in event.metadata['objects']:
+
+                if o['visible'] and o['objectId'] and o['pickupable']:
+                    color = event.object_id_to_color[o['objectId']]
+                    mask = (event.instance_segmentation_frame[:,:,0] == color[0]) & (event.instance_segmentation_frame[:,:,1] == color[1]) &\
+                        (event.instance_segmentation_frame[:,:,2] == color[2])
+                    points = np.argwhere(mask)
+
+                    if len(points) > 0:
+                        min_y = int(np.min(points[:,0]))
+                        max_y = int(np.max(points[:,0]))
+                        min_x = int(np.min(points[:,1]))
+                        max_x = int(np.max(points[:,1]))
+                        max_dim = max((max_y - min_y), (max_x - min_x))
+                        if max_dim > min_size and min_y > buffer and min_x > buffer and max_x < (player_size - buffer) and max_y < (player_size - buffer):
+                            visible_objects.append(dict(objectId=o['objectId'],min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y))
+                            print("[%s] including object id %s %s" % (scene_name, o['objectId'], max_dim))
+
+
+
+
+            if visible_objects:
+                visible_object_locations.append(dict(point=point, rot=rot, hor=hor, visible_objects=visible_objects))
+
+    env.stop()
+    env = ai2thor.controller.Controller()
+    env.start(player_screen_width=zoom_size, player_screen_height=zoom_size)
+    env.reset(scene_name)
+    event = env.step(dict(action='Initialize', gridSize=0.25))
+
+    for o in event.metadata['objects']:
+        if o['receptacle'] and o['receptacleObjectIds'] and o['openable']:
+            print("opening %s" % o['objectId'])
+            env.step(dict(action='OpenObject', objectId=o['objectId'], forceAction=True))
+
+    for vol in visible_object_locations:
+        point = vol['point']
+        
+        event = env.step(dict( action='TeleportFull', x=point['x'], y=point['y'], z=point['z'],rotation=vol['rot'], horizon=vol['hor'], forceAction=True), raise_for_failure=True)
+        for v in vol['visible_objects']:
+            object_id = v['objectId']
+            min_y = int(round(v['min_y'] * (zoom_size/player_size)))
+            max_y = int(round(v['max_y'] * (zoom_size/player_size)))
+            max_x = int(round(v['max_x'] * (zoom_size/player_size)))
+            min_x = int(round(v['min_x'] * (zoom_size/player_size)))
+            delta_y = max_y - min_y
+            delta_x = max_x - min_x
+            scaled_target_size = max(delta_x, delta_y, target_size) + buffer * 2
+            if min_x > (zoom_size - max_x):
+                start_x = min_x - (scaled_target_size - delta_x)
+                end_x = max_x + buffer
+            else:
+                end_x = max_x + (scaled_target_size - delta_x )
+                start_x = min_x - buffer
+
+            if min_y > (zoom_size - max_y):
+                start_y = min_y - (scaled_target_size - delta_y)
+                end_y = max_y + buffer
+            else:
+                end_y = max_y + (scaled_target_size - delta_y)
+                start_y = min_y - buffer
+
+            #print("max x %s max y %s min x %s  min y %s" % (max_x, max_y, min_x, min_y))
+            #print("start x %s start_y %s end_x %s end y %s" % (start_x, start_y, end_x, end_y))
+            print("storing %s " % object_id)
+            img = event.cv2img[start_y: end_y, start_x:end_x, :]
+            seg_img = event.cv2img[min_y: max_y, min_x:max_x, :]
+            dst = cv2.resize(img, (target_size, target_size), interpolation = cv2.INTER_LANCZOS4)
+
+            object_type = object_id.split('|')[0].lower()
+            target_dir = os.path.join("images", scene_name, object_type)
+            h = hashlib.md5()
+            h.update(json.dumps(point, sort_keys=True).encode('utf8'))
+            h.update(json.dumps(v, sort_keys=True).encode('utf8'))
+
+            os.makedirs(target_dir,exist_ok=True)
+
+            cv2.imwrite(os.path.join(target_dir, h.hexdigest() + ".png"), dst)
+
+    env.stop()
+
+    return scene_name
+
+@task
+def build_class_dataset(context):
+    import concurrent.futures 
+    import ai2thor.controller
+    import multiprocessing as mp
+    mp.set_start_method('spawn')
+
+    controller = ai2thor.controller.Controller()
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=4)
+    futures = []
+
+    for scene in controller.scene_names():
+        print("processing scene %s" % scene)
+        futures.append(executor.submit(class_dataset_images_for_scene, scene))
+
+    for f in concurrent.futures.as_completed(futures):
+        scene = f.result()
+        print("scene name complete: %s" % scene)
+
 
 @task
 def local_build(context, prefix='local', arch='OSXIntel64'):
@@ -297,6 +442,8 @@ def poll_ci_build(context):
 @task
 def build(context, local=False):
     from multiprocessing import Process
+    from ai2thor.build import platform_map
+
     version = datetime.datetime.now().strftime('%Y%m%d%H%M')
     build_url_base = 'http://s3-us-west-2.amazonaws.com/%s/' % S3_BUCKET
 
@@ -305,9 +452,6 @@ def build(context, local=False):
     threads = []
     dp = Process(target=build_docker, args=(version,))
     dp.start()
-
-    #for arch in ['OSXIntel64']:
-    platform_map = dict(Linux64="Linux", OSXIntel64="Darwin")
 
     for arch in platform_map.keys():
         unity_path = 'unity'
@@ -530,4 +674,25 @@ def benchmark(ctx, screen_width=600, screen_height=600, editor_mode=False, out='
     with open(out, 'w') as f:
         f.write(json.dumps(benchmark_map, indent=4, sort_keys=True))
 
+    env.stop()
+
+@task
+def test_thing(context, scene, editor_mode=True):
+    import ai2thor.controller
+
+    env = ai2thor.controller.Controller()
+    if editor_mode:
+        env.start(8200, False, player_screen_width=600, player_screen_height=600)
+    else:
+        env.start(player_screen_width=600, player_screen_height=600)
+    env.reset(scene)
+    env.step(
+        dict(
+            action='Initialize',
+            gridSize=0.25,
+            renderNormalsImage=True,
+            renderDepthImage=True,
+            renderClassImage=False,
+            renderObjectImage=True))
+    env.interact()
     env.stop()
