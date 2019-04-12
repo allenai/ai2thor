4ba62bc1c689d4522fa6558ba51ced442da40503
Winson Han <winson.whan@gmail.com>
2019-04-12 15:08:30 -0700

    Liquids put out Candles
    Liquid tagged objects will now put out candles. Can expand this to other on fire objects in the future? Added a new tag for "liquids"

---
+++ unity/Assets/Candle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //if this touches water and it's on, it is put out. Fire safety is important!
+    public void OnTriggerStay(Collider MagiciansRed)
+    {
+        if(MagiciansRed.tag == "Liquid")
+        {
+            if(this.GetComponent<CanToggleOnOff>().isOn)
+            {
+                this.GetComponent<CanToggleOnOff>().Toggle();
+            }
+        }
+    }
+}
