c548489ba84e83bf93adc23224f1e5e804e6f81f
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-04-18 14:05:57 -0700

    Added elements of fix to make first-person character geo invisible from its camera

---
+++ unity/Assets/Scripts/FirstPersonCharacterCull.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FirstPersonCharacterCull : MonoBehaviour
+{
+
+    public MeshRenderer myMeshRenderer; // material you want the camera to change
+
+    void OnPreRender()
+    {
+        //myMeshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        myMeshRenderer.enabled = false;
+    }
+
+    void OnPostRender()
+    {
+        //myMeshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        myMeshRenderer.enabled = true;
+    }
+
+}
