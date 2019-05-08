f92a67803543bcaea0b844098ca2d7b35c94971f
Winson Han <winson.whan@gmail.com>
2019-05-08 15:53:21 -0700

    adjustments to HeatZones
    heatzones can now contextually switch cookable objects into their cooked state, but there are restrictions for microwaves so that microwaves cannot toast bread, because that doesn't make sense and we only have one state of "cooked" bread at the moment.

--- unity/Assets/HeatZone.cs
+++ unity/Assets/HeatZone.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HeatZone : MonoBehaviour
 {
+    //in preparation for different kinds of cooking rather than a single abstracted cooked/uncooked state....
+	//this is to make sure that Microwaves don't switch bread slices into their toasted version, because microwaves can't do that
+	public bool CanToastBread = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +22,9 @@ public class HeatZone : MonoBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        //if any simobjphys are touching this zone, set their temperature values to hot
         if(other.GetComponentInParent<SimObjPhysics>())
         {
+            //Set temperature of object to HOT
             SimObjPhysics sop = other.GetComponentInParent<SimObjPhysics>();
             sop.CurrentTemperature = ObjectMetadata.Temperature.Hot;
 
@@ -28,6 +32,33 @@ public class HeatZone : MonoBehaviour
             sop.HowManySecondsUntilRoomTemp = sop.GetTimerResetValue();
 
             sop.SetStartRoomTempTimer(false);
+            //
+
+            //now if the object is able to be cooked, automatically cook it
+            if(sop.DoesThisObjectHaveThisSecondaryProperty(SimObjSecondaryProperty.CanBeCooked))
+            {
+                CookObject sopcook = sop.GetComponent<CookObject>();
+
+                //if the object is bread... check if CanToastBread is true, and if so proceed to cook
+                if(sop.Type == SimObjType.BreadSliced && CanToastBread == true)
+                {
+                    if(!sopcook.IsCooked())
+                    {
+                        sopcook.Cook();
+                    }
+                }
+
+                //oh it's not bread, no worries just cook it now
+                else if(sop.Type != SimObjType.BreadSliced)
+                {
+                    if(!sopcook.IsCooked())
+                    {
+                        sopcook.Cook();
+                    }
+                }
+            }
+
+
         }
     }
 }
