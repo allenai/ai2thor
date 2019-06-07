4c831dff5d78600b4b43fc844f4f6145373974ba
Eric Kolve <erick@allenai.org>
2019-06-07 14:51:15 -0700

    releasing v2.0.0 - statechanges

--- ai2thor/_builds.py
+++ ai2thor/_builds.py
@@ -1,11 +1,11 @@
 # GENERATED FILE - DO NOT EDIT
-VERSION = '201904041253'
+VERSION = '201906071429'
 BUILDS = {'Darwin': {'build_exception': '',
-            'log': 'thor-201904041253-OSXIntel64.log',
-            'sha256': '7f3081648372b8302e7ef505fc7470d4ad5eb09e17fbadd094b83494f09e3dcb',
-            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201904041253-OSXIntel64.zip'},
- 'Docker': {'tag': '201904041253'},
+            'log': 'thor-201906071429-OSXIntel64.log',
+            'sha256': 'e0dd0370ffb11b35d6bee709e8ab50e51e6917cad727bf2fbcfe5b0220f72ae0',
+            'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201906071429-OSXIntel64.zip'},
+ 'Docker': {'tag': '201906071429'},
  'Linux': {'build_exception': '',
-           'log': 'thor-201904041253-Linux64.log',
-           'sha256': 'f2b6cfda8c1b37c914ff06ec66e062ebfcc2bfc6660b33a63100a16bc19ba82f',
-           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201904041253-Linux64.zip'}}
+           'log': 'thor-201906071429-Linux64.log',
+           'sha256': 'f50c7394567c01f7195c4bd1c2c4c7da58dc14f7f39cea665d0d16d8fbc3cdc7',
+           'url': 'http://s3-us-west-2.amazonaws.com/ai2-thor/builds/thor-201906071429-Linux64.zip'}}
