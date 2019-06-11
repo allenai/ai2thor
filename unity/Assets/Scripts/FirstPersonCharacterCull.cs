13e34e94e87f7eb0579f8e6935c2764e9e29eb35
Winson Han <winson.whan@gmail.com>
2019-06-10 18:12:49 -0700

    some bug fixes to 2.0
    -adjusted missing property on Vase prefab
    -fixed null reference in SceneManager in FloorPlan1 and FloorPlan202
    -fixed agent not appearing when using ToggleMapView action

--- unity/Assets/Scripts/FirstPersonCharacterCull.cs
+++ unity/Assets/Scripts/FirstPersonCharacterCull.cs
@@ -5,28 +5,35 @@ using UnityStandardAssets.Characters.FirstPerson;
 [ExecuteInEditMode]
 public class FirstPersonCharacterCull : MonoBehaviour
 {
-
+    public bool StopCullingThingsForASecond = false;
     public MeshRenderer [] RenderersToHide; //Mesh renderer that you want this script's camera to cull
     public PhysicsRemoteFPSAgentController FPSController;
 
     void OnPreRender() //Just before this camera starts to render...
     {
-        if(FPSController != null && RenderersToHide != null && FPSController.IsVisible)//only do this if visibility capsule has been toggled on
+        if(!StopCullingThingsForASecond)
         {
-            foreach (MeshRenderer mr in RenderersToHide)
+            if(FPSController != null && RenderersToHide != null && FPSController.IsVisible)//only do this if visibility capsule has been toggled on
             {
-                mr.enabled = false; //Turn off renderer
+                foreach (MeshRenderer mr in RenderersToHide)
+                {
+                    mr.enabled = false; //Turn off renderer
+                }
             }
         }
+
     }
 
     void OnPostRender() //Immediately after this camera renders...
     {
-        if(FPSController != null && RenderersToHide != null && FPSController.IsVisible)//only do this if visibility capsule is toggled on
+        if(!StopCullingThingsForASecond)
         {
-            foreach (MeshRenderer mr in RenderersToHide)
+            if(FPSController != null && RenderersToHide != null && FPSController.IsVisible)//only do this if visibility capsule is toggled on
             {
-                mr.enabled = true; //Turn it back on
+                foreach (MeshRenderer mr in RenderersToHide)
+                {
+                    mr.enabled = true; //Turn it back on
+                }
             }
         }
     }
