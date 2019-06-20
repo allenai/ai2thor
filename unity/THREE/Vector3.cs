1ba7759d9e46ec5cb7a3b77aff5b76839c15f42e
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-06-20 15:21:37 -0700

    Removed redundant elements from ShaderForge

---
+++ unity/THREE/Vector3.cs
@@ -0,0 +1,16 @@
+﻿namespace THREE
+{
+    internal class Vector3
+    {
+        private object x;
+        private object y;
+        private object z;
+
+        public Vector3(object x, object y, object z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
