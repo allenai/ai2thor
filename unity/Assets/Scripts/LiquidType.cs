279d0e151b484d37108c706374b28458d6114475
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-08-27 17:25:26 -0700

    Improved wine functionality

--- unity/Assets/Scripts/LiquidType.cs
+++ unity/Assets/Scripts/LiquidType.cs
@@ -26,17 +26,17 @@ public class LiquidProperties {
              }
         },
         {
-            LiquidType.wine, new Liquid { 
-                color = new Color(51 / 255.0f, 0 / 255.0f, 5 / 255.0f, 255 / 255.0f),
-                flowColor =  new Color(106 / 255.0f, 0 / 255.0f, 0 / 255.0f, 230 / 255.0f),
-                topColor = new Color(51 / 255.0f, 0 / 255.0f, 5 / 255.0f, 255 / 255.0f)
+            LiquidType.coffee, new Liquid {
+                color = new Color(62 / 255.0f, 40 / 255.0f, 30 / 255.0f, 255 / 255.0f),
+                flowColor = new Color(119 / 255.0f, 43 / 255.0f, 0 / 255.0f, 198 / 255.0f),
+                topColor = new Color(62 / 255.0f, 40 / 255.0f, 30 / 255.0f, 255 / 255.0f)
             }
         },
         {
-            LiquidType.coffee, new Liquid {
-                color = new Color(62 / 255.0f, 40 / 255.0f, 30 / 255.0f, 255 / 255.0f),
-                flowColor = new Color(113 / 255.0f, 38 / 255.0f, 0 / 255.0f, 19 / 255.0f),
-                topColor = new Color(62 / 255.0f, 40 / 255.0f, 30 / 255.0f, 255 / 255.0f)
+            LiquidType.wine, new Liquid {
+                color = new Color(51 / 255.0f, 0 / 255.0f, 5 / 255.0f, 255 / 255.0f),
+                flowColor =  new Color(106 / 255.0f, 0 / 255.0f, 0 / 255.0f, 230 / 255.0f),
+                topColor = new Color(51 / 255.0f, 0 / 255.0f, 5 / 255.0f, 255 / 255.0f)
             }
         }
     };
