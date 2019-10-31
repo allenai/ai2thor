7cc2268c5573e8553d941bf069b17c73ebab2911
AlvaroHG <alvaroh@allenai.org>
2019-10-31 16:21:44 -0700

    RGB flip false

--- ai2thor/robot_controller.py
+++ ai2thor/robot_controller.py
@@ -105,7 +105,7 @@ class Controller(object):
         for i, agent_metadata in enumerate(payload['metadata']['agents']):
             event = Event(agent_metadata)
             image_mapping = dict(
-                image=lambda x: event.add_image(x, flip_y=False, rb_flip=True),
+                image=lambda x: event.add_image(x, flip_y=False),
                 image_depth=lambda x: event.add_image_depth_meters(
                     x, flip_y=False, flip_x=True, dtype=np.float64
                 )
