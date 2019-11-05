7d96e88013de997b883f81c35213af1836d05df2
Winson Han <winson.whan@gmail.com>
2019-11-04 16:57:33 -0800

    adding light presets to lighting prefab
    adding new 'ChangeLightSet' action to toggle between lighting presets in all Thorkea scenes. THIS WONT WORK IN THE OTHER SCENES ONLY THORKEA RELATED ONES

---
+++ unity/Assets/ChangeLighting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLighting : MonoBehaviour
+{
+    //9 elements right now
+    public GameObject [] Lights;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLights(int lightset)
+    {
+        foreach (GameObject go in Lights)
+        {
+            go.SetActive(false);
+        }
+
+        Lights[lightset - 1].SetActive(true);
+    }
+}
