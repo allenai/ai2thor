36b8fb517cc953dca64a1491c0bf2001f9702fd0
Winson Han <winson.whan@gmail.com>
2019-11-05 18:13:24 -0800

    update lamp prefab, adding in editor screenshot support
    adding `ScreenShotFromCamera` script for staging screenshots in editor. Updating Val2_2 with lamp rotation. Adding colliders in to val floor lamp, also fixing colliders on snillle test chair

---
+++ unity/Assets/Scripts/ScreenShotFromCamera.cs
@@ -0,0 +1,40 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class ScreenShotFromCamera : MonoBehaviour {
+     public int resWidth = 3840; 
+     public int resHeight = 2160;
+ 
+     private bool takeHiResShot = false;
+ 
+     public static string ScreenShotName(int width, int height) {
+         return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
+                              Application.dataPath, 
+                              width, height, 
+                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+     }
+ 
+     public void TakeHiResShot() {
+         takeHiResShot = true;
+     }
+ 
+     void LateUpdate() {
+         takeHiResShot |= Input.GetKeyDown("k");
+         if (takeHiResShot) {
+             RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+             GetComponent<Camera>().targetTexture = rt;
+             Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+             GetComponent<Camera>().Render();
+             RenderTexture.active = rt;
+             screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+             GetComponent<Camera>().targetTexture = null;
+             RenderTexture.active = null; // JC: added to avoid errors
+             Destroy(rt);
+             byte[] bytes = screenShot.EncodeToPNG();
+             string filename = ScreenShotName(resWidth, resHeight);
+             System.IO.File.WriteAllBytes(filename, bytes);
+             Debug.Log(string.Format("Took screenshot to: {0}", filename));
+             takeHiResShot = false;
+         }
+     }
+ }
