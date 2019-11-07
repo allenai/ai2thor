85de9b391cc115735e0472b6fe60b6c56f608493
AlvaroHG <alvaroh@allenai.org>
2019-11-07 14:18:36 -0800

    Cleanup

--- ai2thor/util/scene_yaml_edit.py
+++ ai2thor/util/scene_yaml_edit.py
@@ -1,38 +1,10 @@
 
 import yaml
 from pprint import pprint
-
 from unityparser import UnityDocument
 
 # Requires unity parser, run:
 # pip install unityparser
-
-def removeUnityTagAlias(filepath):
-    """
-    Name:               removeUnityTagAlias()
-
-    Description:        Loads a file object from a Unity textual scene file, which is in a pseudo YAML style, and strips the
-                        parts that are not YAML 1.1 compliant. Then returns a string as a stream, which can be passed to PyYAML.
-                        Essentially removes the "!u!" tag directive, class type and the "&" file ID directive. PyYAML seems to handle
-                        rest just fine after that.
-
-    Returns:                String (YAML stream as string)  
-
-
-    """
-    result = str()
-    sourceFile = open(filepath, 'r')
-
-    for lineNumber,line in enumerate( sourceFile.readlines() ): 
-        if line.startswith('--- !u!'):          
-            result += '--- ' + line.split(' ')[2] + '\n'   # remove the tag, but keep file ID
-        else:
-            # Just copy the contents...
-            result += line
-
-    sourceFile.close()  
-
-    return result
 
 def updateNavMeshParamsForScene(scene_file_name):
     pprint("Updating file '{}'...".format(scene_file_name))
@@ -50,9 +22,9 @@ def updateNavMeshParamsForScene(scene_file_name):
 
     doc.dump_yaml()
 
+
 def GetSceneNames(last_index, last_subIndex, nameTemplate):
     return ["unity/Assets/Scenes/FloorPlan_{}{}_{}.unity".format(nameTemplate,i, j)  for i in range(1, last_index+1) for j in range(1, last_subIndex+1)]
-            
 
 
 def main():
@@ -62,36 +34,6 @@ def main():
     allScenes = testSceneNames + valSceneNames + trainSceneNames
     for scene_file_name in allScenes:
         updateNavMeshParamsForScene(scene_file_name)
-  
-    # fileToLoad = 'unity/Assets/Scenes/test.unity'
-    # UnityStreamNoTags = removeUnityTagAlias(fileToLoad)
-
-    # ListOfNodes = list()
-
-    # for data in yaml.load_all(UnityStreamNoTags):
-    #     ListOfNodes.append( data )
-
-# Example, print each object's name and type
-# for node in ListOfNodes:
-#     if 'm_Name' in node[ node.keys()[0] ]:
-#         print( 'Name: ' + node[ node.keys()[0] ]['m_Name']  + ' NodeType: ' + node.keys()[0] )
-#     else:
-#         print( 'Name: ' + 'No Name Attribute'  + ' NodeType: ' + node.keys()[0] )
-
-#     yaml.add_multi_constructor('!', default_ctor)
-#     # yaml.load(y)
-    
-#     with open('unity/Assets/Scenes/test.unity') as f:
-#         docs = yaml.load_all(f, Loader=yaml.Loader)
-#         for doc in docs:
-#             for k,v in doc.items():
-#                 print(k + "->" + v)
-#                 print("\n")
-#         # doc = yaml.load(f)
-#         # pprint(doc['OcclusionCullingSettings'])
-
-#     with open('file_to_edit.yaml', 'w') as f:
-#         yaml.dump(doc, f)
 
 
 if __name__== "__main__":
--- unity/Assets/Scripts/NavMeshSetup.cs
+++ unity/Assets/Scripts/NavMeshSetup.cs
@@ -13,39 +13,19 @@ public class NavMeshSetup : MonoBehaviour
     public Transform goal;
     private UnityEngine.AI.NavMeshAgent navMeshAgent;
     private Transform hitPos;
-
     private PhysicsRemoteFPSAgentController PhysicsController = null;
-    // Start is called before the first frame update
+
     void Start()
     {
          
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Debug.Log(navMeshAgent.destination);
-
-        // navMeshAgent.destination = new Vector3(goal.position.x, 0.0f, goal.position.z); 
-        // Debug.Log("PAth Status: " + navMeshAgent.pathStatus);
-    }
-
     #if UNITY_EDITOR
-        // [UnityEditor.MenuItem("Thor/Build NavMesh")]
-        // public static void SetCircularLiquidComponent() {
-        //     LiquidPourEdge.SetLiquidComponent("Assets/Prefabs/Systems/CircularLiquidPourEdge.prefab");
-        // }
 
         [UnityEditor.MenuItem("NavMesh/Build NavMeshes for All Scenes")]
         public static void Build()
         {
-            // string[] sceneNames = { "Assets/Scene1.unity", "Assets/Scene2.unity", "Assets/Scene3.unity" };
-            // UnityEditor.AI.NavMeshBuilder.BuildNavMeshForMultipleScenes(sceneNames);
-
-            // UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/Scenes/FloorPlan_Train4_2.unity");
-            // EditorSceneManager.LoadScene("Assets/Scenes/FloorPlan_Train4_2.unity");
-
             var testSceneNames = GetSceneNames(5, 2, "RTest");
             var valSceneNames = GetSceneNames(2, 2, "RVal");
             var trainSceneNames = GetSceneNames(15, 5, "Train");
@@ -57,16 +37,6 @@ public class NavMeshSetup : MonoBehaviour
             selection.AddRange(valSceneNames);
             selection.AddRange(trainSceneNames);
             selection.ToList().ForEach(sceneName => BuildNavmeshForScene(sceneName));
-            //  BuildNavmeshForScene(testSceneNames[0]);
-            // EditorSceneManager.CloseScene(EditorSceneManager.GetActiveScene(), true);
-             //Application.LoadLevel("Assets/Scenes/FloorPlan_Train4_2.unity");
-
-            // var k = 0;
-            // while (k < 200000) {
-            //     k++;
-            // }
-            // EditorSceneManager.OpenScene("Assets/Scenes/FloorPlan_Train2_4.unity");
-            // SceneManager.LoadScene()
         }
 
         private static  List<string> GetSceneNames(int lastIndex, int lastSubIndex, string nameTemplate) {
@@ -95,7 +65,7 @@ public class NavMeshSetup : MonoBehaviour
             var agentController = FindObjectOfType<PhysicsRemoteFPSAgentController>();
             var capsuleCollider = agentController.GetComponent<CapsuleCollider>();
             var navmeshAgent = agentController.GetComponent<NavMeshAgent>();
-            // The Editor bake interface does not take whti parameters and could not be modified as of 2018.3
+            // The Editor bake interface does not take with parameters and could not be modified as of 2018.3
             var buildSettings = new NavMeshBuildSettings() {
                 agentTypeID = navmeshAgent.agentTypeID,
                 agentRadius = navmeshAgent.radius,
@@ -106,41 +76,9 @@ public class NavMeshSetup : MonoBehaviour
                 overrideVoxelSize = false,
                 overrideTileSize = false
             };
-            // UnityEditor.AI.NavMeshBuilder.
 
             int bs = NavMesh.GetSettingsCount();
-     
-        // Loop Through Each Agent Type And Set Height
-        for (int i = 0; i <= bs; ++i)
-        {
-            // Set Agent Types Height
-            // UnityEditor.AI.NavMeshBuilder.CollectSourcesInStage()
-            var settings = NavMesh.GetSettingsByID(i);
-            // settings.agentHeight = 2.0f;
-
-            Debug.Log("Height " + settings.agentHeight + " radius " + settings.agentRadius + " id " + settings.agentTypeID);
-            // NavMesh.GetSettingsByID(i).agentHeight = 10.0f;
-         
-            // Note there is a Name Field
-            // NavMesh.GetSettingsNameFromID(i);
-        }
-
-        // UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
-        // List<NavMeshBuildSource> sources = new List<NavMeshBuildSource>();
-        // NavMeshBuilder.CollectSources(
-        //     new Bounds(BoundsCenter, BoundsSize),
-        //     mask,
-        //     NavMeshCollectGeometry.PhysicsColliders,
-        //     0,
-        //     new List<NavMeshBuildMarkup>(),
-        //     sources);
-        // NavMeshBuilder.CollectSources()
-        // NavMeshBuilder.BuildNavMeshData(buildSettings, )
-
-        // navMeshSurface.BuildNavMesh();
-        // UnityEditor.AI.NavMeshAssetManager.instance.StartBakingSurfaces()
-
-            // EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+    
             UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
             EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
         }
@@ -159,10 +97,6 @@ public class NavMeshSetup : MonoBehaviour
                      UnityEditor.GameObjectUtility.SetNavMeshArea(meshRenderer.gameObject, NavMesh.GetAreaFromName("Not Walkable"));
                  });
                  Debug.Log("Setting flag for " + child.gameObject.name + " layer " +  NavMesh.GetAreaFromName("Not Walkable"));
-                 
-                //  Debug.Log("Setting flag for " + child.gameObject.name + " layer " +  NavMesh.GetAreaFromName("Not Walkable"));
-                //  UnityEditor.GameObjectUtility.SetStaticEditorFlags(child.gameObject,  UnityEditor.StaticEditorFlags.NavigationStatic);
-                //  UnityEditor.GameObjectUtility.SetNavMeshArea(child.gameObject, NavMesh.GetAreaFromName("Not Walkable"));
              }
         }
     #endif
