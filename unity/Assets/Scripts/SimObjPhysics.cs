7d1198d121b5e78bdd56792198e79d4d91a7033c
Winson Han <winson.whan@gmail.com>
2019-12-06 15:51:52 -0800

    removing debug message

--- unity/Assets/Scripts/SimObjPhysics.cs
+++ unity/Assets/Scripts/SimObjPhysics.cs
@@ -745,10 +745,10 @@ public class SimObjPhysics : MonoBehaviour, SimpleSimObj
             Debug.LogError(this.name + " is not at uniform scale! Set scale to (1, 1, 1)!!!");
         }
 
-        if(BoundingBox == null)
-        {
-            Debug.LogError(this.name + "AAAAAAAHHH NO BOX");
-        }
+        // if(BoundingBox == null)
+        // {
+        //     Debug.LogError(this.name + "AAAAAAAHHH NO BOX");
+        // }
 #endif
 		//end debug setup stuff
 
