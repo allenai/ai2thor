29897e58e7c7818637d8c230fbbf81507e3ad6ee
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-12-11 16:10:58 -0800

    Added 360 screenshot tool prototype

---
+++ unity/Assets/Scripts/Screenshot360.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class Screenshot360 : MonoBehaviour
+{
+    public int imageWidth = 4096;
+    public bool saveAsJPEG = true;
+    public string savePath;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            byte[] bytes = I360Render.Capture(imageWidth, saveAsJPEG);
+            if (bytes != null)
+            {
+                string path = Path.Combine(savePath, "360render" + (saveAsJPEG ? ".jpeg" : ".png"));
+                File.WriteAllBytes(path, bytes);
+                Debug.Log("360 render saved to " + path);
+            }
+        }
+    }
+}
