db05737f506a23a29c10c13813fdb63ee19fb0b6
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2019-12-12 11:34:46 -0800

    Updated Screenshot360 to be a menu item instead of just a hotkey

--- unity/Assets/Scripts/Screenshot360.cs
+++ unity/Assets/Scripts/Screenshot360.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Screenshot360 : MonoBehaviour
 {
-    public int imageWidth = 4096;
-    public bool saveAsJPEG = true;
-    int currentCount;
-    bool newFileName;
-    string path;
-
-    void Update()
+    
+    [MenuItem("GameObject/Take 360 Screenshot %p")]
+    private static void Generate360Screenshot()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            byte[] bytes = I360Render.Capture(imageWidth, saveAsJPEG);
-            newFileName = false;
-            currentCount = 1;
+        int imageWidth = 4096;
+        bool saveAsJPEG = true;
+        bool newFileName = false;
+        int currentCount = 1;
+        string path;
 
+        byte[] bytes = I360Render.Capture(imageWidth, saveAsJPEG);
+            
             if (bytes != null)
             {
                 while (!newFileName)
@@ -31,13 +30,11 @@ public class Screenshot360 : MonoBehaviour
                     else
                     {
                         path = Path.Combine("Assets/360Photos", "360Render_" + UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + "_" + currentCount + (saveAsJPEG ? ".jpeg" : ".png"));
+                        File.WriteAllBytes(path, bytes);
+                        Debug.Log("360 render saved to " + path);
                         newFileName = true;
                     }
                 }
-                    File.WriteAllBytes(path, bytes);
-                    Debug.Log("360 render saved to " + path);
-                    currentCount++;
             }
         }
-    }
 }
