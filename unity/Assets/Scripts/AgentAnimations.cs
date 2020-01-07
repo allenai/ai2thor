4814790bd67c60077cdd15c05419e4e085fab0cc
mattdeitke <mdeitke23@gmail.com>
2020-01-06 18:28:50 -0800

    minor text and grid updates

--- unity/Assets/Scripts/AgentAnimations.cs
+++ unity/Assets/Scripts/AgentAnimations.cs
@@ -29,12 +29,43 @@ public class AgentAnimations : PhysicsRemoteFPSAgentController
 				Quaternion.Euler(new Vector3(gridLine.transform.rotation.x, 90, gridLine.transform.rotation.z)));
 			newLine.transform.localScale = new Vector3(10, newLine.transform.localScale.y, newLine.transform.localScale.z);
         }*/
-    }
 
-    //int i = 0;
+		int GridSize = 25;
 
-    // Update is called once per frame
-    void Update()
+        MeshFilter filter = gameObject.GetComponent<MeshFilter>();
+        var mesh = new Mesh();
+        var verticies = new List<Vector3>();
+
+        var indicies = new List<int>();
+        for (int i = 0; i < GridSize; i++)
+        {
+            verticies.Add(new Vector3(i, 0, 0));
+            verticies.Add(new Vector3(i, 0, GridSize));
+
+            indicies.Add(4 * i + 0);
+            indicies.Add(4 * i + 1);
+
+            verticies.Add(new Vector3(0, 0, i));
+            verticies.Add(new Vector3(GridSize, 0, i));
+
+            indicies.Add(4 * i + 2);
+            indicies.Add(4 * i + 3);
+        }
+
+        mesh.vertices = verticies.ToArray();
+        mesh.SetIndices(indicies.ToArray(), MeshTopology.Lines, 0);
+        filter.mesh = mesh;
+
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        meshRenderer.material.color = Color.white;
+
+}
+
+//int i = 0;
+
+// Update is called once per frame
+void Update()
     {
         //a.rotateDegrees = a.rotateDegrees + 5.0f;
         //Debug.Log(a.rotateDegrees);
