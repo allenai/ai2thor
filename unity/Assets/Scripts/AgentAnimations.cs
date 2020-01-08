94b0da82ab4b9661986e8a215e18a1d6f3aa12d7
mattdeitke <mdeitke23@gmail.com>
2020-01-08 11:28:55 -0800

    add basic object animations

--- unity/Assets/Scripts/AgentAnimations.cs
+++ unity/Assets/Scripts/AgentAnimations.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class AgentAnimations : PhysicsRemoteFPSAgentController 
+public class AgentAnimations : PhysicsRemoteFPSAgentController
 {
 
     [SerializeField] private PhysicsRemoteFPSAgentController agent;
     [SerializeField] private ServerAction a;
 	public GameObject gridLine;
-
-    public bool yugi = false;
 
 	// Start is called before the first frame update
 	void Start()
@@ -19,9 +17,6 @@ public class AgentAnimations : PhysicsRemoteFPSAgentController
 		a.moveMagnitude = 0.1f;
 		gridSize = 0.01f;
         a.rotateDegrees = 5.0f;
-
-        collisionsInAction = new List<string>();
-
 		//agent.LookUp(a);
         /*
         for (int i = 0; i < 11; i++) {
@@ -35,36 +30,6 @@ public class AgentAnimations : PhysicsRemoteFPSAgentController
 			newLine.transform.localScale = new Vector3(10, newLine.transform.localScale.y, newLine.transform.localScale.z);
         }*/
 
-		int GridSize = 25;
-
-        MeshFilter filter = gameObject.GetComponent<MeshFilter>();
-        var mesh = new Mesh();
-        var verticies = new List<Vector3>();
-
-        var indicies = new List<int>();
-        for (int i = 0; i < GridSize; i++)
-        {
-            verticies.Add(new Vector3(i, 0, 0));
-            verticies.Add(new Vector3(i, 0, GridSize));
-
-            indicies.Add(4 * i + 0);
-            indicies.Add(4 * i + 1);
-
-            verticies.Add(new Vector3(0, 0, i));
-            verticies.Add(new Vector3(GridSize, 0, i));
-
-            indicies.Add(4 * i + 2);
-            indicies.Add(4 * i + 3);
-        }
-
-        mesh.vertices = verticies.ToArray();
-        mesh.SetIndices(indicies.ToArray(), MeshTopology.Lines, 0);
-        filter.mesh = mesh;
-
-        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
-        meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        meshRenderer.material.color = Color.white;
-
 }
 
 //int i = 0;
@@ -72,13 +37,6 @@ public class AgentAnimations : PhysicsRemoteFPSAgentController
 // Update is called once per frame
 void Update()
     {
-        if(yugi)
-        {
-            Vector3 moveDirection = Vector3.zero;
-            moveDirection.y -= (-Physics.gravity.y * Time.deltaTime * m_GravityMultiplier);
-            
-            m_CharacterController.Move(moveDirection * Time.deltaTime);
-        }
         //a.rotateDegrees = a.rotateDegrees + 5.0f;
         //Debug.Log(a.rotateDegrees);
         //agent.RotateDegrees(a);
@@ -95,15 +53,6 @@ void Update()
         //agent.MoveAhead(a);
         //agent.MoveAhead(a);
         //agent.actionComplete = false;
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-        if(other.name == "YouveActivatedMyTrapCard" && yugi == false)
-        {
-            yugi = true;
-            other.gameObject.SetActive(false);
-        }
     }
 
 }
--- unity/Assets/Scripts/SimpleSceneRandomObjectPositions.cs
+++ unity/Assets/Scripts/SimpleSceneRandomObjectPositions.cs
@@ -6,68 +6,97 @@ using System;
 
 public class SimpleSceneRandomObjectPositions : MonoBehaviour
 {
-    public GameObject[] prefabs;
     public GameObject apple;
-    public GameObject pit;
+    public GameObject trap;
     public GameObject interiorWall;
     public GameObject robot;
     public GameObject objectsParent;
 
-	// get the bounds of the scene
-    public GameObject maxXWall;
-    public GameObject maxZWall;
-    public GameObject minXWall;
-    public GameObject minZWall;
+    public GameObject floor;
+
+    private GameObject appleInstance;
 
     // Start is called before the first frame update
     void Start()
     {
-        //UnityEngine.Random.InitState(42);
-        int maxX = Mathf.FloorToInt(maxXWall.transform.position.x) - 1;
-        int maxZ = Mathf.FloorToInt(maxZWall.transform.position.z) - 1;
-        int minX = Mathf.CeilToInt(minXWall.transform.position.x) + 1;
-        int minZ = Mathf.CeilToInt(minZWall.transform.position.z) + 1;
+        UnityEngine.Random.InitState(42);
+        int minX = 1;
+        int minZ = -10;
+        int maxX = 10;
+        int maxZ = -1;
+
+        // currently only 1 interior wall since with multiple it may
+        // make it impossible to reach the apple in a scene
+        // (maybe replace interiorWall with multiple wall panels?)
+        GameObject[] objects = { apple, trap, interiorWall, robot };
 
         // generate a list of all possible positions
         ArrayList combinations = new ArrayList();
         for (int i = minX; i <= maxX; i++) {
             for (int j = minZ; j <= maxZ; j++) {
-                int[] arr = new int[] {i, j};
+                int[] arr = {i, j};
                 combinations.Add(arr);
 			}
 		}
 
         // generate the random int positions
         ArrayList pairs = new ArrayList();
-        //System.Random rand = new System.Random(42);
-        System.Random rand = new System.Random();
-        for (int i = 0; i < prefabs.Length + 1; i++) {
+        System.Random rand = new System.Random(47);
+        //System.Random rand = new System.Random();
+        for (int i = 0; i < objects.Length; i++) {
             int nextIdx = (int) rand.Next(0, combinations.Count);
             pairs.Add(combinations[nextIdx]);
             combinations.RemoveAt(nextIdx);
 		}
 
-        foreach (GameObject prefab in prefabs) {
+        // set each object to a random, unique grid position
+        foreach (GameObject prefab in objects) {
+            // extracts the {x, z} position
             int[] nextPos = (int[]) pairs[0];
-            Debug.Log(nextPos[0]);
-            Debug.Log(nextPos[1]);
 
-            var newObject = Instantiate(prefab,
-                new Vector3(nextPos[0], prefab.transform.position.y, nextPos[1]),
-                Quaternion.Euler(new Vector3(prefab.transform.rotation.x, UnityEngine.Random.Range(0, 4) * 90, prefab.transform.rotation.z))
-			);
-            newObject.transform.parent = objectsParent.transform;
+            if (prefab == robot) {
+                // don't add the robot object, since it's already in the scene
+                prefab.transform.position = new Vector3(nextPos[0] - 0.5f, prefab.transform.position.y, nextPos[1] + 0.5f);
+                prefab.transform.localRotation = Quaternion.Euler(new Vector3(prefab.transform.rotation.x, UnityEngine.Random.Range(0, 4) * 90, prefab.transform.rotation.z));
+            } else {
+                // adds the prefab to the scene
+                // uses delta 0.5f to center the object in the grid cell
+                var newObject = Instantiate(prefab,
+				    new Vector3(nextPos[0] - 0.5f, prefab.transform.position.y, nextPos[1] + 0.5f),
+					Quaternion.Euler(new Vector3(prefab.transform.rotation.x, UnityEngine.Random.Range(0, 4) * 90, prefab.transform.rotation.z))
+			    );
+
+                // sets the parent of the prefab
+                newObject.transform.parent = objectsParent.transform;
+
+                if (prefab == apple) {
+                    appleInstance = newObject;
+				}
+
+                if (prefab == trap) {
+                    // destroy the floor piece covering the trap
+                    // hard coded in solution based on how the floor's children are ordered
+                    Destroy(floor.transform.GetChild(
+						(nextPos[0] + 1) * 12 - 2 - (10 - Math.Abs(nextPos[1]))).gameObject
+					);
+                }
+            }
+
+			// moves to the next random pair
             pairs.RemoveAt(0);
 		}
-        int[] lastPos = (int[]) pairs[0];
-        robot.transform.position = new Vector3(lastPos[0], robot.transform.position.y, lastPos[1]);
-        robot.transform.rotation = Quaternion.Euler(new Vector3(robot.transform.rotation.x, UnityEngine.Random.Range(0, 4) * 90, robot.transform.rotation.z));
-
     }
 
+    int frame = 1;
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = 0.1f;
+        appleInstance.transform.position = new Vector3(
+			appleInstance.transform.position.x,
+			1.25f + 0.2f * (float) Math.Sin(speed * frame),
+			appleInstance.transform.position.z);
+        frame++;
+        //appleInstance.transform.position = new Vector3(0, 0, 0);
     }
 }
