9de6680b91aaa29260a103bbb5e7211930cff24d
mattdeitke <mdeitke23@gmail.com>
2020-01-16 17:53:57 -0800

    move to video yield

--- ai2thor/video_controller.py
+++ ai2thor/video_controller.py
@@ -8,13 +8,18 @@ from queue import Queue
 
 # create many threads that work at the same time
 
+# need transparent skybox/background
+
+# also need agentType stochastic to work
+
+# spawn in objects (talk to Luca?)
+
 class VideoController(ai2thor.controller.Controller):
 	def __init__(self, **controller_kwargs):
 		super().__init__(**controller_kwargs)
-		# controller_kwargs['agentType'] = 'stochastic'
 		# super().__init__(**controller_kwargs)
 
-		# add 3rd party camera
+		# add 3rd party camera... todo: make adding a 3rd party camera an action
 		self.step(
 			action='AddThirdPartyCamera', 
 			rotation=dict(x=0, y=90, z=0), 
@@ -22,62 +27,66 @@ class VideoController(ai2thor.controller.Controller):
 			fieldOfView=60)
 
 		self.saved_frames = []
-		self.upcoming_camera_animations = Queue()
 
-	def vstep(self, **step_kwargs):
+	def transform(self, *action_generators: list):
+		# action_generators should be a list of generators (e.g., moveAhead(<Params>))
+		# this does many transformations at the same time
+		
+		while True:
+			# execute next actions if available
+			next_actions = [next(generator, False) for generator in action_generators]
 
-		# update the camera
-		if not self.upcoming_camera_animations.empty():
-			next_cam_anim_kwargs = self.upcoming_camera_animations.get()
-			self.step(next_cam_anim_kwargs)
-		pass
+			# add the frame to the saved frames after all actions execute
+			self.saved_frames.append(self.last_event.third_party_camera_frames[0])
+
+			# remove actions with finished iterators
+			next_actions = [action for action in next_actions if action != False]
+			print('|', end='')
+			if not next_actions:
+				# all generators have finished
+				break
+		print()
 
 	def moveAhead(self, moveMagnitude=1, frames=60):
 		for _ in range(frames):
-			event = self.step(action='MoveAhead', moveMagnitude=moveMagnitude / frames)
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='MoveAhead', moveMagnitude=moveMagnitude / frames)
 
 	def moveBack(self, moveMagnitude=1, frames=60):
 		for _ in range(frames):
-			event = self.step(action='MoveBack', moveMagnitude=moveMagnitude / frames)
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='MoveBack', moveMagnitude=moveMagnitude / frames)
 
 	def moveLeft(self, moveMagnitude=1, frames=60):
 		for _ in range(frames):
-			event = self.step(action='MoveLeft', moveMagnitude=moveMagnitude / frames)
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='MoveLeft', moveMagnitude=moveMagnitude / frames)
 
 	def moveRight(self, moveMagnitude=1, frames=60):
 		for _ in range(frames):
-			event = self.step(action='MoveRight', moveMagnitude=moveMagnitude / frames)
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='MoveRight', moveMagnitude=moveMagnitude / frames)
 
 	def rotateRight(self, rotateDegrees=90, frames=60):
-		# do teleporting
+		# do incremental teleporting
 		pass
 
 	def rotateLeft(self, rotateDegrees=90, frames=60):
-		# do teleporting
+		# do incremental teleporting
 		pass
 
 	def Pass(self, frames=60):
 		for _ in range(frames):
-			event = self.step(action='Pass')
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='Pass')
 
 	def relativeCameraAnimation(self, px=0, py=0, pz=0, rx=0, ry=0, rz=0, frames=60):
 		for _ in range(frames):
 			cam = self.last_event.metadata['thirdPartyCameras'][0]
 			pos, rot = cam['position'], cam['rotation']
-			event = self.step(action='UpdateThirdPartyCamera',
-					  		  thirdPartyCameraId=0,
-					  		  rotation={'x': rot['x'] + rx / frames,
-					  		  			'y': rot['y'] + ry / frames,
-					  		  			'z': rot['z'] + rz / frames},
-					  		  position={'x': pos['x'] + px / frames,
-					  			        'y': pos['y'] + py / frames,
-					  			        'z': pos['z'] + pz / frames})
-			self.saved_frames.append(event.third_party_camera_frames[0])
+			yield self.step(action='UpdateThirdPartyCamera',
+							thirdPartyCameraId=0,
+							rotation={'x': rot['x'] + rx / frames,
+									  'y': rot['y'] + ry / frames,
+									  'z': rot['z'] + rz / frames},
+							position={'x': pos['x'] + px / frames,
+									  'y': pos['y'] + py / frames,
+									  'z': pos['z'] + pz / frames})
 
 	def absoluteCameraAnimation(self):
 		pass
@@ -91,14 +100,23 @@ class VideoController(ai2thor.controller.Controller):
 	def FocusOnPoint(self):
 		pass
 
+	def stand(self):
+		pass
+
+	def crouch(self):
+		pass
+
 	def exportVideo(self, path):
+		# merges all the saved frames into a mp4 video and saves it
 		if self.saved_frames:
 			path = path if path[:-4] == '.mp4' else path + '.mp4'
 			if os.path.exists(path):
 				os.remove(path)
-			video = cv2.VideoWriter(path, cv2.VideoWriter_fourcc(*'avc1'), 30, (300, 300))
-			for frame in self.saved_frames:
-				print('i', end=', ')
+			# sets the size of the video to (300, 300) -- TODO: make based on saved_frames[0].size
+			video = cv2.VideoWriter(path, cv2.VideoWriter_fourcc(*'avc1'), 30, (self.saved_frames[0].shape[1], self.saved_frames[0].shape[0]))
+			for i, frame in enumerate(self.saved_frames):
+				print(i, end=', ')
+				# assumes that the frames are RGB images. CV2 uses BGR.
 				video.write(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 			video.release()
 			print('done')
