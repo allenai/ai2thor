9befa92d519568fac4f30a631ac92ec9cce73757
mattdeitke <mdeitke23@gmail.com>
2020-02-20 14:14:38 -0800

    prepare for github actions on commits

--- ai2thor/THORboard/tic_tac_toe.py
+++ ai2thor/THORboard/tic_tac_toe.py
@@ -2,6 +2,19 @@ import torch
 
 class TicTacToe:
     def __init__(self, starting_player=1):
+        """
+        example:
+            ```from ai2thor.THORboard import TicTacToe
+            game = TicTacToe()```
+
+        description:
+            This module consists of the functionality to play a standard game
+            of TicTacToe 
+
+        params:
+            `starting_player` | int | Takes on the values of either 0 or 1 and
+                indicates which player has the starting move. | 1
+        """
         assert starting_player == 1 or starting_player == -1
         self.has_next_move = starting_player
 
@@ -9,37 +22,61 @@ class TicTacToe:
         self.board = torch.zeros((3, 3))
 
     def get_valid_actions(self):
-        # extracts all game positions for a particular player
-        # self.has_next_move will be either 1 or -1
+        """
+        example:
+            `game.get_valid_actions()`
+
+        exampleOut:
+            `[(0, 0), (0, 1), (1, 1), (2, 1)]`
+
+        description:
+            Extracts all board positions where the next player can play.
+        """
         positions = (self.board == 0).nonzero()
         return [(int(p[0]), int(p[1])) for p in positions]
 
-    def step(self, pos: tuple):
-        # draw on position should be a tuple from get_valid_actions
-        assert pos in self.get_valid_actions()
+    def step(self, play_on: tuple):
+        """
+        example:
+            `game.step(play_on=(0, 1))`
+
+        description:
+            Throws a ValueError if the given position is not on the board or
+            has already been played on.
+
+        
+        """
+        if play_on not in self.get_valid_actions():
+            raise ValueError('Invalid position')
 
         # make move
-        row, col = pos
+        row, col = play_on
         self.board[row, col] = self.has_next_move 
 
         # change the player
         self.has_next_move *= -1
 
     def game_over(self):
-        """Returns 1 if p1 wins, -1 if p2 wins, and 0 if game is not over.
-           
-           Assumes only one player is the winner.
-           If the board is something like:
-               1 | _ | -1
-               1 | _ | -1
-               1 | _ | -1
-           there may be unexpected results."""
+        """
+        example:
+            `game.game_over()`
+
+        description:
+            Returns 1 if p1 wins, -1 if p2 wins, and 0 if game is not over.
+            Assumes only one player is the winner.
+            If the board is something like:
+                 1 | _ | -1
+                 1 | _ | -1
+                 1 | _ | -1
+            there may be unexpected results.
+        """
         b = self.board
 
         def determine_winner(tensor):
-            # for each triplet, returns 1 if p1 wins, -1 if p2 wins,
-            # and 0 if undetermined
-
+            """
+                For each triplet, returns 1 if p1 wins, -1 if p2 wins, and 0
+                if undetermined.
+            """
             # game not over
             if abs(sum(tensor)) != 3:
                 return 0
