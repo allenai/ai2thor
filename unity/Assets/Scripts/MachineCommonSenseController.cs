3ea9f18f6d6b4d809d9a418cb1588306817beea3
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2020-04-02 16:54:06 -0400

    MCS-107 Fixed position, distance, heading, and direction in output object metadata.

--- unity/Assets/Scripts/MachineCommonSenseController.cs
+++ unity/Assets/Scripts/MachineCommonSenseController.cs
@@ -163,6 +163,16 @@ public class MachineCommonSenseController : PhysicsRemoteFPSAgentController {
         objectMetadata.points = (meshFilter != null && meshFilter.mesh != null) ? meshFilter.mesh.vertices :
             new Vector3[] { };
 
+        // Use the object's renderer (each object should have a renderer, except maybe shelf children in complex
+        // receptacle objects) for its position because its transform's position may not be its actual position
+        // in the camera since the renderer may be defined in a child object with an offset position.
+        Renderer renderer = simObj.gameObject.GetComponentInChildren<Renderer>();
+        if (renderer != null) {
+            objectMetadata.position = renderer.bounds.center;
+            // Use the object's new position for the distance previously set in generateObjectMetadata.
+            objectMetadata.distance = Vector3.Distance(this.transform.position, objectMetadata.position);
+        }
+
         // From https://docs.unity3d.com/Manual/DirectionDistanceFromOneObjectToAnother.html
         objectMetadata.heading = objectMetadata.position - this.transform.position;
         objectMetadata.direction = (objectMetadata.heading / objectMetadata.heading.magnitude);
