eb3a68165748b78e47b88cc029567954159df3d9
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2020-05-06 14:53:58 -0700

    Added GroupComand script

---
+++ unity/Assets/Scripts/GroupCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class GroupCommand
+{
+    [MenuItem("GameObject/Group Selected %g")]
+    private static void GroupSelected()
+    {
+        if (!Selection.activeTransform) return;
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+        Selection.activeGameObject = go;
+    }
+}
