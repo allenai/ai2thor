eca92b4331b8255091e8f94d35262c3473907f76
Eric Kolve <erick@allenai.org>
2020-06-17 09:12:39 -0700

    setting __version__ to travis_tag in build-deploy

--- scripts/build_deploy.sh
+++ scripts/build_deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+if [[ -z "$TRAVIS_TAG" ]]; then
+    echo "TRAVIS_TAG must be set for build/deploy"
+    exit 1
+fi
+
+# we need to have __version__ for pip3 install to succeed
+# it also will get set in build-pip with additional sanity checks
+echo "__version__ = '$TRAVIS_TAG'" >> ai2thor/_version.py
+
 pip3 install -e .
 pip3 install twine wheel invoke
 
