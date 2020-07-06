01376ea61242e9f2895fb6eebedc6fd8bcd7cf99
Jacob Audick <jaudick@caci2006277-ja.hq.nextcentury.com>
2020-07-06 17:06:55 -0400

    Testing setup implemented

---
+++ unity/Assets/Scripts/TestScripts/PlayMode/PlayModeTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+
+namespace Tests {
+    public class PlayModeTest
+    {
+        private GameObject gameObject;
+        private MCSMain main;
+
+        [SetUp]
+        public void Setup(){
+            gameObject = GameObject.Instantiate(new GameObject());
+            main = gameObject.AddComponent<MCSMain>();
+
+        }
+        
+
+        [UnityTest]
+        public IEnumerator FirstTest() {
+            yield return null;
+            Assert.AreEqual(false, false);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GameObject.Destroy(gameObject);
+        }
+    
+    }
+}
