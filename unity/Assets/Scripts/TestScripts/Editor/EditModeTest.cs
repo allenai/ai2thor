752c03bcfd1852ddf6384324f0652144f3618fdc
Jacob Audick <jaudick@caci2006277-ja.hq.nextcentury.com>
2020-07-08 09:07:59 -0400

    Testing Framework Setup

---
+++ unity/Assets/Scripts/TestScripts/Editor/EditModeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EditModeTest
+    {
+        private GameObject gameObject;
+        private MCSMain main;
+
+        [SetUp]
+        public void Setup()
+        {
+            gameObject = GameObject.Instantiate(new GameObject());
+            main = gameObject.AddComponent<MCSMain>();
+        }
+        
+
+        [Test]
+        public void FirstTest() 
+        {
+            Assert.AreEqual(true, true);
+            Assert.IsFalse(false);
+            Assert.False(false);        }
+    }
+}
