947d3a747b90873f5163b8eb3223389b76b371c7
winthos <winson.whan@gmail.com>
2020-07-21 13:05:14 -0700

    fix to ExpRoom_Arm FPSController prefab
    -arm heirarchy and colliders set up
    -updating FPSController prefab to be in line with the most recent bot/tall/drone version of the prefab

--- unity/Assets/IK_Robot_Arm_Controller.cs
+++ unity/Assets/IK_Robot_Arm_Controller.cs
@@ -15,4 +15,24 @@ public class IK_Robot_Arm_Controller : MonoBehaviour
     {
         
     }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        //debug collision print
+        //print(collision.collider);
+        //print(collision.gameObject);
+
+        if(collision.gameObject.GetComponent<SimObjPhysics>())
+        {
+            SimObjPhysics sop = collision.gameObject.GetComponent<SimObjPhysics>();
+            if(sop.PrimaryProperty == SimObjPrimaryProperty.Static)
+            {
+                //stop moving flag here
+                //something like check the stop moving flag while in moveArm xyz coroutine
+                //if coroutine needs to stop, then reset the stop moving flag?
+                //the timing between this physics onCollisionEnter update and the coroutine update might be a problem... we'll see?
+            }
+        }
+    }
+
 }
