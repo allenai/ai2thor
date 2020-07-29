03ab2d26db6bc6f895f1dbc8026b31661193cd98
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2020-07-28 20:00:45 -0700

    Fixed additional bugs on Manipulator_Clamp script

--- unity/Assets/RobotArmTest/Manipulator_Clamp.cs
+++ unity/Assets/RobotArmTest/Manipulator_Clamp.cs
@@ -24,33 +24,30 @@ public class Manipulator_Clamp : MonoBehaviour
     
     void Update()
     {
-        
+        //Debug.Log("Checking for hemisphere change!");
         if (transform.position != prevCoordinates[0])
         {
-            //Debug.Log("Checking for hemisphere change!");
-            if (transform.position != prevCoordinates[0])
+            //Clamp to front hemisphere
+            rootToWrist = rootJoint.InverseTransformPoint(transform.position);
+
+            //Debug.Log("Z from " + rootJoint.gameObject.name + " to " + transform.gameObject.name + ": " + rootToWrist);
+            rootToShoulder = shoulderJoint.localPosition;
+            //Debug.Log("Z from " + rootJoint.gameObject.name + " to shoulder: " + shoulderJoint.gameObject.name + ": " + rootToShoulder);
+            if (rootToWrist.z - 0.01f <= rootToShoulder.z)
             {
-                //Clamp to front hemisphere
-                rootToWrist = rootJoint.InverseTransformPoint(transform.position);
+                Debug.Log("Wrist is behind shoulder!");
+                transform.position += rootJoint.TransformDirection(Vector3.forward * (rootToShoulder.z - rootToWrist.z + 0.01f));
+            }
 
-                //Debug.Log("Z from " + rootJoint.gameObject.name + " to " + transform.gameObject.name + ": " + rootToWrist);
-                rootToShoulder = shoulderJoint.localPosition;
-                //Debug.Log("Z from " + rootJoint.gameObject.name + " to shoulder: " + shoulderJoint.gameObject.name + ": " + rootToShoulder);
-                if (rootToWrist.z - 0.01f <= rootToShoulder.z)
-                {
-                    Debug.Log("Wrist is behind shoulder!");
-                    transform.position += rootJoint.TransformDirection(Vector3.forward * (rootToShoulder.z - rootToWrist.z + 0.01f));
-                }
-
-                //Clamp manipulator to radius
-                //Maximum shoulder-to-wrist length is ~0.6335!!!
-                shoulderToWrist = rootToWrist - rootToShoulder;
-                //Debug.Log(shoulderToWrist.magnitude + " vs " + shoulderJoint.InverseTransformPoint(transform.position).magnitude);
-                if (shoulderToWrist.sqrMagnitude >= Mathf.Pow(0.6335f, 2))
-                {
-                    //Debug.Log("Arm is overreaching!");
-                    transform.position += rootJoint.TransformDirection((shoulderToWrist.normalized * 0.6325f) - shoulderToWrist);
-                }
+            rootToWrist = rootJoint.InverseTransformPoint(transform.position);
+            //Clamp manipulator to radius
+            //Maximum shoulder-to-wrist length is ~0.6335!!!
+            shoulderToWrist = rootToWrist - rootToShoulder;
+            //Debug.Log(shoulderToWrist.magnitude + " vs " + shoulderJoint.InverseTransformPoint(transform.position).magnitude);
+            if (shoulderToWrist.sqrMagnitude >= Mathf.Pow(0.6335f, 2) && shoulderToWrist.z > 0)
+            {
+                //Debug.Log("Arm is overreaching!");
+                transform.position += rootJoint.TransformDirection((shoulderToWrist.normalized * 0.6325f) - shoulderToWrist);
             }
         }
 
