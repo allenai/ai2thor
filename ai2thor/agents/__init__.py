0e1561a3e278d6968dadf07e8fc556789953315c
Matt Deitke <mdeitke23@gmail.com>
2020-08-17 14:47:48 -0700

    add full base agent

--- ai2thor/agents/__init__.py
+++ ai2thor/agents/__init__.py
@@ -2,7 +2,7 @@
 TODO: Give Warning on action fail!
 """
 import ai2thor
-from typing import Union
+from typing import Union, Tuple, Dict
 
 
 class Agent:
@@ -13,26 +13,77 @@ class Agent:
         self.controller = controller
         self.agent_idx = agent_idx
 
-    def _step(self, **action_kwargs) -> None:
-        pass
+    @property
+    def last_event(self):
+        return self.controller.last_event
+
+    @property
+    def pose(self) -> Dict[str, int]:
+        # use this format for teleporting
+        raise NotImplementedError()
+
+    @property
+    def pos(self) -> Tuple[float, float, float]:
+        # should only provide degrees of freedom that can change
+        raise NotImplementedError()
+
+    @property
+    def rot(self) -> Tuple[float, float, float]:
+        # should only provide degrees of freedom that can change
+        raise NotImplementedError()
+
+    def _step(self, action: str, **action_kwargs) -> None:
+        # single agent
+        if self.agent_idx is None:
+            self.controller.step(action, **action_kwargs)
+
+        # multi-agent
+        else:
+            self.controller.step(
+                action,
+                agentId=self.agent_idx,
+                **action_kwargs)
 
     def done(self) -> None:
+        """Updates last_event without changing the environment"""
         self._step(action='done')
-        pass
 
     def rotate(self, degrees: float = 90, direction: str = 'right') -> None:
-        pass
+        """Translates the agent in 'direction' by 'degrees'"""
+        valid_directions = {'right', 'left'}
+        if direction not in valid_directions:
+            raise ValueError(f'direction must be in {valid_directions}')
+
+        if direction == 'right':
+            self._step(action='RotateRight', degrees=degrees)
+        else:
+            self._step(action='RotateLeft', degrees=degrees)
 
     def move(self, meters: float = 0.25, direction: str = 'ahead') -> None:
-        pass
+        """Translates the agent in 'direction' by a distance of 'meters'"""
+        valid_directions = {'ahead', 'right', 'left', 'back'}
+        if direction not in valid_directions:
+            raise ValueError(f'direction must be in {valid_directions}')
 
-    def teleport(
-            self,
-            x: Union[None, float] = None,
-            z: Union[None, float] = None,
-            rot_y: Union[None, float] = None,
-            horizon: Union[None, float] = None,
-            y: Union[None, float] = None,
-            rot_x: Union[None, float] = None,
-            rot_z: Union[None, float] = None) -> None:
-        pass
+        kwargs = {'moveMagnitude': meters}
+        if direction == 'ahead':
+            self._step(action='MoveAhead', **kwargs)
+        elif direction == 'right':
+            self._step(action='MoveRight', **kwargs)
+        elif direction == 'left':
+            self._step(action='MoveLeft', **kwargs)
+        elif direction == 'back':
+            self._step(action='MoveBack', **kwargs)
+
+    def peak(self, direction: str = 'up', degrees: float = 30) -> None:
+        """Rotates the agent's head in 'direction' by 'degrees' without
+           rotating its body."""
+        valid_directions = {'up', 'down'}
+        if direction not in valid_directions:
+            raise ValueError(f'direction must be in {valid_directions}')
+
+        kwargs = {'degrees': degrees}
+        if direction == 'up':
+            self._step(action='LookUp', **kwargs)
+        elif direction == 'down':
+            self._step(action='LookDown', **kwargs)
