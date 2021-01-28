ff98f2491d292066d06889aad16a54c759c08cfd
Matt Deitke <mdeitke23@gmail.com>
2021-01-27 20:29:21 -0800

    remove old volume code -- it appears elsewhere

--- unity/Assets/Scripts/PhysicsRemoteFPSAgentController.cs
+++ unity/Assets/Scripts/PhysicsRemoteFPSAgentController.cs
@@ -6041,46 +6041,6 @@ namespace UnityStandardAssets.Characters.FirstPerson {
             actionFinished(true);
         }
 
-        // Following code for calculating the volume of a mesh taken from
-        // https://answers.unity.com/questions/52664/how-would-one-calculate-a-3d-mesh-volume-in-unity.html
-        // and https://answers.unity.com/questions/52664/how-would-one-calculate-a-3d-mesh-volume-in-unity.html
-        //  protected float SignedVolumeOfTriangle(Vector3 p1, Vector3 p2, Vector3 p3) {
-        //     float v321 = p3.x * p2.y * p1.z;
-        //     float v231 = p2.x * p3.y * p1.z;
-        //     float v312 = p3.x * p1.y * p2.z;
-        //     float v132 = p1.x * p3.y * p2.z;
-        //     float v213 = p2.x * p1.y * p3.z;
-        //     float v123 = p1.x * p2.y * p3.z;
-        //     return (1.0f / 6.0f) * (-v321 + v231 + v312 - v132 - v213 + v123);
-        // }
-        // protected float VolumeOfMesh(Mesh mesh) {
-        //     float volume = 0;
-        //     Vector3[] vertices = mesh.vertices;
-        //     int[] triangles = mesh.triangles;
-        //     Debug.Log(vertices);
-        //     Debug.Log(triangles);
-        //     for (int i = 0; i < mesh.triangles.Length; i += 3)
-        //     {
-        //         Vector3 p1 = vertices[triangles[i + 0]];
-        //         Vector3 p2 = vertices[triangles[i + 1]];
-        //         Vector3 p3 = vertices[triangles[i + 2]];
-        //         volume += SignedVolumeOfTriangle(p1, p2, p3);
-        //     }
-        //     return Mathf.Abs(volume);
-        // }
-
-        // public void VolumeOfObject(ServerAction action) {
-        //     SimObjPhysics so = physicsSceneManager.ObjectIdToSimObjPhysics[action.objectId];
-        //     foreach (MeshFilter meshFilter in so.GetComponentsInChildren<MeshFilter>()) {
-        //         Mesh mesh = meshFilter.sharedMesh;
-        //         float volume = VolumeOfMesh(mesh);
-        //         string msg = "The volume of the mesh is " + volume + " cube units.";
-        //         Debug.Log(msg);
-        //     }
-        // }
-
-        // End code for calculating the volume of a mesh
-
         // @pOpen is the probability of opening an openable object.
         // @randOpenness specifies if the openness for each opened object should be random, between 0% : 100%, or always 100%.
         public void RandomlyOpenCloseObjects(
