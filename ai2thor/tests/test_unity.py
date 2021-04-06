6b3621e42f549cd655e723a32ad45daf62169ce6
Eric Kolve <erick@allenai.org>
2021-04-06 12:19:05 -0700

    fixing test_2d_semantic_hulls pytest.mark.parametrize

--- ai2thor/tests/test_unity.py
+++ ai2thor/tests/test_unity.py
@@ -1182,7 +1182,7 @@ def test_get_interactable_poses(controller):
     ), "GetInteractablePoses with large maxDistance is off!"
 
 
-@pytest.mark.parametrize("controller", [wsgi_controller, fifo_controller])
+@pytest.mark.parametrize("controller", fifo_wsgi)
 def test_2d_semantic_hulls(controller):
     controller.reset('FloorPlan28')
     obj_name_to_obj_id = {o["name"]: o["objectId"] for o in controller.last_event.metadata["objects"]}
