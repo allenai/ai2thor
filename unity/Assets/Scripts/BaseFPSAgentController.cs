3c7d27649a0c3b6c14460268651f9ac4d3e2c76e
Eric Kolve <erick@allenai.org>
2021-04-12 13:51:42 -0700

    Making massThreshold nullable and moving MakeAllObjectsMoveable to the SceneManager; calling MakeAllObjectsMoveable on Initialize when the arm is used

--- unity/Assets/Scripts/BaseFPSAgentController.cs
+++ unity/Assets/Scripts/BaseFPSAgentController.cs
@@ -1282,25 +1282,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         //this means that the isSceneAtRest bool will always be false
         public void MakeAllObjectsMoveable()
         {
-            foreach (SimObjPhysics sop in GameObject.FindObjectsOfType<SimObjPhysics>()) 
-            {
-                //check if the sopType is something that can be hung
-                if(sop.Type == SimObjType.Towel || sop.Type == SimObjType.HandTowel || sop.Type == SimObjType.ToiletPaper)
-                {
-                    //if this object is actively hung on its corresponding object specific receptacle... skip it so it doesn't fall on the floor
-                    if(sop.GetComponentInParent<ObjectSpecificReceptacle>())
-                    {
-                        continue;
-                    }
-                }
-
-                if (sop.PrimaryProperty == SimObjPrimaryProperty.CanPickup || sop.PrimaryProperty == SimObjPrimaryProperty.Moveable) 
-                {
-                    Rigidbody rb = sop.GetComponent<Rigidbody>();
-                    rb.isKinematic = false;
-                    rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
-                }
-            }
+            physicsSceneManager.MakeAllObjectsMoveable();
             actionFinished(true);
         }
 
