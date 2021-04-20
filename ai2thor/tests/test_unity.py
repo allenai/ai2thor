a59643ba359e3f99c44f6caa954ce5cc8e701c0e
mattdeitke <mdeitke23@gmail.com>
2021-04-20 15:36:50 -0700

    add arm json schema

--- ai2thor/tests/test_unity.py
+++ ai2thor/tests/test_unity.py
@@ -90,16 +90,9 @@ def fifo_controller():
 fifo_wsgi = [_fifo_controller, _wsgi_controller]
 fifo_wsgi_stoch = [_fifo_controller, _wsgi_controller, _stochastic_controller]
 
-BASE_FP28_POSITION = dict(
-    x=-1.5,
-    z=-1.5,
-    y=0.901,
-)
+BASE_FP28_POSITION = dict(x=-1.5, z=-1.5, y=0.901,)
 BASE_FP28_LOCATION = dict(
-    **BASE_FP28_POSITION,
-    rotation={"x": 0, "y": 0, "z": 0},
-    horizon=0,
-    standing=True,
+    **BASE_FP28_POSITION, rotation={"x": 0, "y": 0, "z": 0}, horizon=0, standing=True,
 )
 
 
@@ -204,9 +197,7 @@ def test_deprecated_segmentation_params(fifo_controller):
     # renderClassImage has been renamed to renderSemanticSegmentation
 
     fifo_controller.reset(
-        TEST_SCENE,
-        renderObjectImage=True,
-        renderClassImage=True,
+        TEST_SCENE, renderObjectImage=True, renderClassImage=True,
     )
     event = fifo_controller.last_event
     with warnings.catch_warnings():
@@ -223,9 +214,7 @@ def test_deprecated_segmentation_params2(fifo_controller):
     # renderClassImage has been renamed to renderSemanticSegmentation
 
     fifo_controller.reset(
-        TEST_SCENE,
-        renderSemanticSegmentation=True,
-        renderInstanceSegmentation=True,
+        TEST_SCENE, renderSemanticSegmentation=True, renderInstanceSegmentation=True,
     )
     event = fifo_controller.last_event
 
@@ -624,9 +613,7 @@ def test_open_interactable_with_filter(controller):
     controller.step(dict(action="SetObjectFilter", objectIds=[]))
     assert controller.last_event.metadata["objects"] == []
     controller.step(
-        action="OpenObject",
-        objectId=fridge["objectId"],
-        raise_for_failure=True,
+        action="OpenObject", objectId=fridge["objectId"], raise_for_failure=True,
     )
 
     controller.step(dict(action="ResetObjectFilter", objectIds=[]))
@@ -658,9 +645,7 @@ def test_open_interactable(controller):
     assert fridge["visible"], "Object is not interactable!"
     assert_near(controller.last_event.metadata["agent"]["position"], position)
     event = controller.step(
-        action="OpenObject",
-        objectId=fridge["objectId"],
-        raise_for_failure=True,
+        action="OpenObject", objectId=fridge["objectId"], raise_for_failure=True,
     )
     fridge = next(
         obj
@@ -982,6 +967,16 @@ def test_jsonschema_metadata(controller):
 
 
 @pytest.mark.parametrize("controller", fifo_wsgi)
+def test_arm_jsonschema_metadata(controller):
+    controller.reset(agentMode="arm")
+    event = controller.step(action="Pass")
+    with open(os.path.join(TESTS_DATA_DIR, "arm-metadata-schema.json")) as f:
+        schema = json.loads(f.read())
+
+    jsonschema.validate(instance=event.metadata, schema=schema)
+
+
+@pytest.mark.parametrize("controller", fifo_wsgi)
 def test_get_scenes_in_build(controller):
     scenes = set()
     for g in glob.glob("unity/Assets/Scenes/*.unity"):
@@ -1052,8 +1047,7 @@ def test_teleport(controller):
     # Teleporting too high
     before_position = controller.last_event.metadata["agent"]["position"]
     controller.step(
-        "Teleport",
-        **{**BASE_FP28_LOCATION, "y": 1.0},
+        "Teleport", **{**BASE_FP28_LOCATION, "y": 1.0},
     )
     assert not controller.last_event.metadata[
         "lastActionSuccess"
@@ -1064,8 +1058,7 @@ def test_teleport(controller):
 
     # Teleporting into an object
     controller.step(
-        "Teleport",
-        **{**BASE_FP28_LOCATION, "z": -3.5},
+        "Teleport", **{**BASE_FP28_LOCATION, "z": -3.5},
     )
     assert not controller.last_event.metadata[
         "lastActionSuccess"
@@ -1073,8 +1066,7 @@ def test_teleport(controller):
 
     # Teleporting into a wall
     controller.step(
-        "Teleport",
-        **{**BASE_FP28_LOCATION, "z": 0},
+        "Teleport", **{**BASE_FP28_LOCATION, "z": 0},
     )
     assert not controller.last_event.metadata[
         "lastActionSuccess"
