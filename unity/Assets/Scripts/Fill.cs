0c39b7c11689e4c4242fb39c8363f05687a26fab
Matt Deitke <mdeitke23@gmail.com>
2021-05-23 22:29:46 -0700

    make sure all paths fail with an errorMessage

--- unity/Assets/Scripts/Fill.cs
+++ unity/Assets/Scripts/Fill.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 public class Fill : MonoBehaviour {
     [SerializeField]
@@ -11,7 +13,6 @@ public class Fill : MonoBehaviour {
 
     [SerializeField]
     protected GameObject WineObject = null;
-
 
     [SerializeField]
     protected bool isFilled = false; // false - empty, true - currently filled with
@@ -38,7 +39,6 @@ public class Fill : MonoBehaviour {
         Liquids.Add("water", WaterObject);
         Liquids.Add("coffee", CoffeeObject);
         Liquids.Add("wine", WineObject);
-
     }
 
     // Update is called once per frame
@@ -50,127 +50,64 @@ public class Fill : MonoBehaviour {
                 EmptyObject();
             }
         }
-
-        // // debug stuff
-        // if(Input.GetKeyDown(KeyCode.G))
-        // {
-        //     FillObject("water");
-        // }
     }
 
     // fill the object with a random liquid
     public void FillObjectRandomLiquid() {
         int whichone = Random.Range(1, 3);
-        if (whichone == 1) {
-            FillObject("water");
-        }
-
-        if (whichone == 2) {
-            FillObject("wine");
-        }
-
-        if (whichone == 3) {
-            FillObject("coffee");
+        switch (whichone) {
+            case 1:
+                FillObject("water");
+                break;
+            case 2:
+                FillObject("wine");
+                break;
+            case 3:
+                FillObject("coffee");
+                break;
         }
     }
 
-    public bool FillObject(string whichLiquid) {
-        if (Liquids.ContainsKey(whichLiquid)) {
-            // check if this object has whichLiquid setup as fillable: If the object has a null reference this object
-            // is not setup for that liquid
-            if (Liquids[whichLiquid] == null) {
-                return false;
-            }
-
-            Liquids[whichLiquid].transform.gameObject.SetActive(true);
-
-            // coffee is hot so change the object's temperature if whichLiquid was coffee
-            if (whichLiquid == "coffee") {
-                // coffee is hot!
-                SimObjPhysics sop = gameObject.GetComponent<SimObjPhysics>();
-                sop.CurrentTemperature = ObjectMetadata.Temperature.Hot;
-                if (sop.HowManySecondsUntilRoomTemp != sop.GetTimerResetValue()) {
-                    sop.HowManySecondsUntilRoomTemp = sop.GetTimerResetValue();
-                }
-
-                sop.SetStartRoomTempTimer(false);
-            }
-
-            isFilled = true;
-            currentlyFilledWith = whichLiquid;
-            return true;
+    public void FillObject(string whichLiquid) {
+        if (!Liquids.ContainsKey(whichLiquid)) {
+            throw new ArgumentException("Unknown liquid: " + whichLiquid);
         }
 
-        // whichLiquid is not in the dictionary
-        else {
-            return false;
+        // check if this object has whichLiquid setup as fillable: If the object has a null reference this object
+        // is not setup for that liquid
+        if (Liquids[whichLiquid] == null) {
+            throw new ArgumentException(
+                $"The liquid {whichLiquid} is not setup for this object."
+            );
         }
 
+        Liquids[whichLiquid].transform.gameObject.SetActive(true);
 
-        // if the dict doesn't contain this key pair uuuuuh
+        // coffee is hot so change the object's temperature if whichLiquid was coffee
+        if (whichLiquid == "coffee") {
+            // coffee is hot!
+            SimObjPhysics sop = gameObject.GetComponent<SimObjPhysics>();
+            sop.CurrentTemperature = ObjectMetadata.Temperature.Hot;
+            if (sop.HowManySecondsUntilRoomTemp != sop.GetTimerResetValue()) {
+                sop.HowManySecondsUntilRoomTemp = sop.GetTimerResetValue();
+            }
+            sop.SetStartRoomTempTimer(false);
+        }
 
-        // if(whichLiquid == "water")
-        // {
-        //     if(WaterObject != null)
-        //     WaterObject.transform.gameObject.SetActive(true);
-
-        //     isFilled = true;
-        //     currentlyFilledWith = "water";
-        // }
-
-        // else if(whichLiquid == "coffee")
-        // {
-        //     if(CoffeeObject != null)
-        //     CoffeeObject.transform.gameObject.SetActive(true);
-
-        //     // coffee is hot!
-        //     SimObjPhysics sop = gameObject.GetComponent<SimObjPhysics>();
-        //     sop.CurrentTemperature = ObjectMetadata.Temperature.Hot;
-        //     if(sop.HowManySecondsUntilRoomTemp != sop.GetTimerResetValue())
-        //     sop.HowManySecondsUntilRoomTemp = sop.GetTimerResetValue();
-        //     sop.SetStartRoomTempTimer(false);
-
-
-        //     isFilled = true;
-        //     currentlyFilledWith = "coffee";
-        // }
-
-        // else if(whichLiquid == "wine")
-        // {
-        //     if(WineObject != null)
-        //     CoffeeObject.transform.gameObject.SetActive(true);
-
-        //     isFilled = true;
-        //     currentlyFilledWith = "wine";
-        // }
+        isFilled = true;
+        currentlyFilledWith = whichLiquid;
     }
 
     public void EmptyObject() {
         // for each thing in Liquids, if it exists set it to false and then set bools appropriately
-
         foreach (KeyValuePair<string, GameObject> gogogo in Liquids) {
             // if the value field is not null and has a reference to a liquid object 
             if (gogogo.Value != null) {
                 gogogo.Value.SetActive(false);
             }
         }
-        // Liquids[currentlyFilledWith].transform.gameObject.SetActive(false);
         currentlyFilledWith = null;
         isFilled = false;
-
-        // if(currentlyFilledWith == "water")
-        // {
-        //     WaterObject.transform.gameObject.SetActive(false);
-        //     currentlyFilledWith = null;
-        //     isFilled= false;
-        // }
-
-        // else if(currentlyFilledWith == "coffee")
-        // {
-        //     CoffeeObject.transform.gameObject.SetActive(false);
-        //     currentlyFilledWith = null;
-        //     isFilled= false;
-        // }
     }
 
     public void OnTriggerStay(Collider other) {
