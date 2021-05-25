5dfa0abc0fd871d8641639624cd7da71a062f855
Erick <erick.lopez@unity3d.com>
2021-05-25 17:15:31 -0400

    Adding methods for clearing addressables cache and downloading all addressables. Adding new custom objects asset group for adding custom objects locally to builds.

--- unity/Assets/Scripts/Addressables/AddressablesUtil.cs
+++ unity/Assets/Scripts/Addressables/AddressablesUtil.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 /// <summary>
 /// 
@@ -26,6 +30,22 @@ public class AddressablesUtil : MonoBehaviour
     {
         Instance = this;
         Addressables.InitializeAsync();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                ClearAllAddressablesCache();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                CacheAllAddressables();
+            }
+        }
     }
 
     /// <summary>
@@ -67,7 +87,7 @@ public class AddressablesUtil : MonoBehaviour
         AsyncOperationHandle<GameObject> objectOperation = Addressables.LoadAssetAsync<GameObject>(path);
         GameObject objectAsset = objectOperation.WaitForCompletion();
 
-        var op = Addressables.InstantiateAsync(path);
+        AsyncOperationHandle<GameObject> op = Addressables.InstantiateAsync(path);
         GameObject objectInstance = op.WaitForCompletion();
         addressableGameObjects.Add(objectInstance);
 
@@ -86,5 +106,112 @@ public class AddressablesUtil : MonoBehaviour
             Addressables.ReleaseInstance(go);
         }
         addressableGameObjects.Clear();
+    }
+
+    /// <summary>
+    /// Clears all downloaded addressables cached on local machine
+    /// </summary>
+    public void ClearAllAddressablesCache()
+    {
+        Debug.LogError("Clearing Cache");
+
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+        stopWatch.Start();
+
+        StartCoroutine(ClearAllCoroutine(stopWatch));
+    }
+
+    private IEnumerator ClearAllCoroutine(System.Diagnostics.Stopwatch stopWatch)
+    {
+        yield return Addressables.InitializeAsync();
+        var handle = ClearAllAddressablesCacheHandle();
+        yield return handle;
+
+        // Everything has finished, log the time it took
+        stopWatch.Stop();
+
+        System.TimeSpan ts = stopWatch.Elapsed;
+        string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Debug.LogError("Cache Cleared : " + elapsedTime);
+    }
+
+    private AsyncOperationHandle<bool> ClearAllAddressablesCacheHandle()
+    {
+        var locs = GetAllAddressablesLocations();
+        return Addressables.ClearDependencyCacheAsync(locs, true);
+    }
+
+    /// <summary>
+    /// Downloads and caches all addressable bundles on local machine
+    /// </summary>
+    public void CacheAllAddressables()
+    {
+        Debug.LogError("Caching All Addressables");
+
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+        stopWatch.Start();
+
+        StartCoroutine(DownloadAllCoroutine(stopWatch));
+    }
+
+    private IEnumerator DownloadAllCoroutine(System.Diagnostics.Stopwatch stopWatch)
+    {
+        yield return Addressables.InitializeAsync();
+        var handle = CacheAllAddressablesHandle();
+        yield return handle;
+        Caching.ClearCache();
+
+        // Everything has finished, log the time it took
+        stopWatch.Stop();
+
+        System.TimeSpan ts = stopWatch.Elapsed;
+        string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Debug.LogError("Addressables Cached! : " + elapsedTime);
+    }
+
+    private AsyncOperationHandle<IList<IAssetBundleResource>> CacheAllAddressablesHandle()
+    {
+        var locs = GetAllAddressablesLocations();
+        foreach (var location in locs)
+            Debug.Log("Will download : " + location.InternalId);
+        return Addressables.LoadAssetsAsync<IAssetBundleResource>(locs, null, true);
+    }
+
+    private List<IResourceLocation> GetAllAddressablesLocations()
+    {
+        List<IResourceLocation> locations = new List<IResourceLocation>();
+        HashSet<int> addedLocations = new HashSet<int>();
+        foreach (IResourceLocator locator in Addressables.ResourceLocators)
+        {
+            var map = locator as ResourceLocationMap;
+            if (map == null)
+                continue;
+            foreach (KeyValuePair<object, IList<IResourceLocation>> mapLocation in map.Locations)
+            {
+                foreach (IResourceLocation location in mapLocation.Value)
+                {
+                    int hash = location.Hash(location.ResourceType);
+                    if (addedLocations.Contains(hash))
+                        continue;
+                    if (typeof(IAssetBundleResource).IsAssignableFrom(location.ResourceType))
+                    {
+                        var dls = Addressables.GetDownloadSizeAsync(location);
+                        dls.WaitForCompletion();
+                        
+                        // this does not seem to be returning different results
+                        //if (dls.Result > 0)
+                        {
+                            locations.Add(location);
+                            addedLocations.Add(hash);
+                        }
+                    }
+                }
+            }
+        }
+        return locations;
     }
 }
