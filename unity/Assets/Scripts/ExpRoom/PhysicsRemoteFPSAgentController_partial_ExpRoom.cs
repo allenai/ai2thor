07fac4f0ed6abf1f3b875fe8ba6a4d2c4d278aab
lucaw <lucaw@allenai.org>
2021-07-09 14:33:13 -0700

    Improvements to usability of exproom action.

--- unity/Assets/Scripts/ExpRoom/PhysicsRemoteFPSAgentController_partial_ExpRoom.cs
+++ unity/Assets/Scripts/ExpRoom/PhysicsRemoteFPSAgentController_partial_ExpRoom.cs
@@ -21,14 +21,14 @@ namespace UnityStandardAssets.Characters.FirstPerson {
             while (enumerator.MoveNext()) { }
         }
 
-        public void WhichAvailableObjectsFitInAvailableContainers() {
+        public void WhichContainersDoesAvailableObjectFitIn(string objectName) {
             PhysicsSceneManager.StartPhysicsCoroutine(
                 startCoroutineUsing: this,
-                enumerator: whichAvailableObjectsFitInAvailableContainers()
+                enumerator: whichContainersDoesAvailableObjectFitIn(objectName)
             );
         }
 
-        public IEnumerator<float?> whichAvailableObjectsFitInAvailableContainers() {
+        public IEnumerator<float?> whichContainersDoesAvailableObjectFitIn(string objectName) {
             GameObject availableObjectsGameObject = GameObject.Find("AvailableObjects");
             GameObject availableContainersGameObject = GameObject.Find("AvailableContainers");
 
@@ -40,157 +40,165 @@ namespace UnityStandardAssets.Characters.FirstPerson {
 
             Action<SimObjPhysics> deactivateSop = (sop) => {
                 sop.gameObject.SetActive(false);
-                physicsSceneManager.ObjectIdToSimObjPhysics.Remove(sop.ObjectID);
+                physicsSceneManager.RemoveFromObjectsInScene(sop);
             };
 
             Vector3 middleOfTable = new Vector3(-0.5f, 0.9f, 0.2f);
             Vector3 rightOfTable = new Vector3(0.534f, 0.9f, 0.2f);
 
-            Dictionary<string, Dictionary<string, float>> objNameToCoverNames = new Dictionary<string, Dictionary<string, float>>();
+            Dictionary<string, float> coverNameToScale = new Dictionary<string, float>();
 
+            SimObjPhysics toCover = null;
             foreach (Transform toCoverTransform in availableObjectsGameObject.transform) {
-                SimObjPhysics toCover = toCoverTransform.gameObject.GetComponent<SimObjPhysics>();
-                objNameToCoverNames[toCover.name] = new Dictionary<string, float>();
+                SimObjPhysics sop = toCoverTransform.gameObject.GetComponent<SimObjPhysics>();
+                if (sop.name == objectName) {
+                    toCover = sop;
+                    break;
+                }
+            }
 
-                if (toCover.GetComponent<Break>()) {
-                    toCover.GetComponent<Break>().Unbreakable = true;
+            if (toCover == null) {
+                errorMessage = $"Could not find object with name {objectName}";
+                actionFinished(false);
+                yield break;
+            }
+
+            if (toCover.GetComponent<Break>()) {
+                toCover.GetComponent<Break>().Unbreakable = true;
+            }
+
+            activateSop(toCover);
+
+            if (!PlaceObjectAtPoint(
+                target: toCover,
+                position: middleOfTable,
+                rotation: null,
+                forceKinematic: true
+            )) {
+                deactivateSop(toCover);
+                errorMessage = $"{toCover.name} failed to place";
+                actionFinished(false);
+                yield break;
+            }
+
+            yield return 0f;
+
+            Vector3 toCoverPos = toCover.transform.position;
+
+            int numberFit = 0;
+            foreach (Transform coverTransform in availableContainersGameObject.transform) {
+                SimObjPhysics cover = coverTransform.gameObject.GetComponent<SimObjPhysics>();
+                if (cover.GetComponent<Break>()) {
+                    cover.GetComponent<Break>().Unbreakable = true;
                 }
 
-                activateSop(toCover);
+                cover.transform.rotation = Quaternion.Euler(180f, 0f, 0f);
+                activateSop(cover);
 
-                if (!PlaceObjectAtPoint(
-                    target: toCover,
-                    position: middleOfTable,
-                    rotation: null,
-                    forceKinematic: true
-                )) {
-#if UNITY_EDITOR
-                    Debug.Log($"{toCover.name} failed to place");
-#endif
-                    deactivateSop(toCover);
-                    continue;
-                }
+                BoxCollider coverBbox = cover.BoundingBox.GetComponent<BoxCollider>();
+                float maxScale = Mathf.Min(
+                    0.5f / coverBbox.size.y,
+                    0.7f / coverBbox.size.x,
+                    0.7f / coverBbox.size.z,
+                    1.75f
+                );
+                float minScale = Mathf.Min(0.5f, maxScale / 2.0f);
 
-                yield return 0f;
-
-                Vector3 toCoverPos = toCover.transform.position;
-
-                int numberFit = 0;
-                foreach (Transform coverTransform in availableContainersGameObject.transform) {
-                    SimObjPhysics cover = coverTransform.gameObject.GetComponent<SimObjPhysics>();
-                    if (cover.GetComponent<Break>()) {
-                        cover.GetComponent<Break>().Unbreakable = true;
-                    }
-
-                    cover.transform.rotation = Quaternion.Euler(180f, 0f, 0f);
-                    activateSop(cover);
-
-                    BoxCollider coverBbox = cover.BoundingBox.GetComponent<BoxCollider>();
-                    float maxScale = Mathf.Min(
-                        0.5f / coverBbox.size.y,
-                        0.7f / coverBbox.size.x,
-                        0.7f / coverBbox.size.z,
-                        1.75f
-                    );
-                    float minScale = Mathf.Min(0.5f, maxScale / 2.0f);
-
-                    float lastScale = 1.0f;
-                    Func<float, bool> tryScale = (scale) => {
-                        emptyEnumerator(scaleObject(
-                            scale: scale / lastScale,
-                            target: cover,
-                            scaleOverSeconds: 0f,
-                            skipActionFinished: true
-                        ));
-                        lastScale = scale;
-
-                        Physics.SyncTransforms();
-
-                        if (!PlaceObjectAtPoint(
-                            target: cover,
-                            position: rightOfTable,
-                            rotation: null,
-                            forceKinematic: true,
-                            includeErrorMessage: true
-                        )) {
-#if UNITY_EDITOR
-                            Debug.Log($"{cover.name} failed to place: {errorMessage}");
-#endif
-                            deactivateSop(cover);
-                            return false;
-                        }
-
-                        float coverY = cover.transform.position.y;
-                        cover.transform.position = new Vector3(
-                            toCoverPos.x,
-                            coverY,
-                            toCoverPos.z
-                        );
-
-                        Physics.SyncTransforms();
-
-                        Collider coverCollidingWith = UtilityFunctions.firstColliderObjectCollidingWith(
-                            go: cover.gameObject
-                        );
-                        if (coverCollidingWith == null) {
-                            bool toCoverVisible = isSimObjVisible(m_Camera, toCover, 10f);
-                            if (!toCoverVisible) {
-                                return true;
-                            }
-                        }
-                        // else {
-                        //     Debug.Log($"{cover.name} colliding with {coverCollidingWith.transform.parent.name}");
-                        // }
-                        return false;
-                    };
-
-                    if (tryScale(maxScale)) {
-                        for (int i = 0; i <= 5; i++) {
-                            float newScale = (minScale + maxScale) / 2.0f;
-                            if (tryScale(newScale)) {
-                                maxScale = newScale;
-                            } else {
-                                minScale = newScale;
-                            }
-                            yield return null;
-                        }
-#if UNITY_EDITOR
-                        Debug.Log($"{toCover.name} fits under {cover.name} at {maxScale} scale");
-#endif
-                        objNameToCoverNames[toCover.name][cover.name] = maxScale;
-                        numberFit += 1;
-                    }
-
+                float lastScale = 1.0f;
+                Func<float, bool> tryScale = (scale) => {
                     emptyEnumerator(scaleObject(
-                        scale: 1.0f / lastScale,
+                        scale: scale / lastScale,
                         target: cover,
                         scaleOverSeconds: 0f,
                         skipActionFinished: true
                     ));
+                    lastScale = scale;
 
-                    deactivateSop(cover);
+                    Physics.SyncTransforms();
+
+                    if (!PlaceObjectAtPoint(
+                        target: cover,
+                        position: rightOfTable,
+                        rotation: null,
+                        forceKinematic: true,
+                        includeErrorMessage: true
+                    )) {
+#if UNITY_EDITOR
+                        Debug.Log($"{cover.name} failed to place: {errorMessage}");
+#endif
+                        deactivateSop(cover);
+                        return false;
+                    }
+
+                    float coverY = cover.transform.position.y;
+                    cover.transform.position = new Vector3(
+                        toCoverPos.x,
+                        coverY,
+                        toCoverPos.z
+                    );
+
+                    Physics.SyncTransforms();
+
+                    Collider coverCollidingWith = UtilityFunctions.firstColliderObjectCollidingWith(
+                        go: cover.gameObject
+                    );
+                    if (coverCollidingWith == null) {
+                        bool toCoverVisible = isSimObjVisible(m_Camera, toCover, 10f);
+                        if (!toCoverVisible) {
+                            return true;
+                        }
+                    }
+                    // else {
+                    //     Debug.Log($"{cover.name} colliding with {coverCollidingWith.transform.parent.name}");
+                    // }
+                    return false;
+                };
+
+                if (tryScale(maxScale)) {
+                    for (int i = 0; i <= 5; i++) {
+                        float newScale = (minScale + maxScale) / 2.0f;
+                        if (tryScale(newScale)) {
+                            maxScale = newScale;
+                        } else {
+                            minScale = newScale;
+                        }
+                        yield return null;
+                    }
+#if UNITY_EDITOR
+                    Debug.Log($"{toCover.name} fits under {cover.name} at {maxScale} scale");
+#endif
+                    coverNameToScale[cover.name] = maxScale;
+                    numberFit += 1;
                 }
 
-#if UNITY_EDITOR
-                Debug.Log(
-                    Newtonsoft.Json.JsonConvert.SerializeObject(
-                        objNameToCoverNames,
-                        Newtonsoft.Json.Formatting.None,
-                        new Newtonsoft.Json.JsonSerializerSettings() {
-                            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
-                            ContractResolver = new ShouldSerializeContractResolver()
-                        }
-                    )
-                );
-#endif
+                emptyEnumerator(scaleObject(
+                    scale: 1.0f / lastScale,
+                    target: cover,
+                    scaleOverSeconds: 0f,
+                    skipActionFinished: true
+                ));
 
-                deactivateSop(toCover);
-
-                yield return 0f;
-                break;
+                deactivateSop(cover);
             }
 
-            actionFinished(true, objNameToCoverNames);
+#if UNITY_EDITOR
+            Debug.Log(
+                Newtonsoft.Json.JsonConvert.SerializeObject(
+                    coverNameToScale,
+                    Newtonsoft.Json.Formatting.None,
+                    new Newtonsoft.Json.JsonSerializerSettings() {
+                        ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                        ContractResolver = new ShouldSerializeContractResolver()
+                    }
+                )
+            );
+#endif
+
+            deactivateSop(toCover);
+
+            yield return 0f;
+
+            actionFinished(true, coverNameToScale);
         }
 
         public void AvailableExpRoomObjects() {
