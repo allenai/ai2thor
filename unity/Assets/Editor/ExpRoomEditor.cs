58bf22c0b9aa0d3abe5fd8c3b43479ecc8d2a228
lucaw <lucaw@allenai.org>
2021-07-08 17:24:31 -0700

    Adding ExpRoom specific scripts and updates.

---
+++ unity/Assets/Editor/ExpRoomEditor.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using System.Security.Cryptography;
+
+
+public class ExpRoomEditor : EditorWindow {
+    Vector2 scroll;
+
+    List <(SimObjPhysics, string)> sopAndPrefabTuples = null;
+
+    [MenuItem("ExpRoom/Add all pickupable prefabs to AvailableObjects")]
+    static void AddPickupableToAvailableObjects() {
+        GameObject availableObjectsGameObject = GameObject.Find("AvailableObjects");
+        AddAllPrefabsToAsOnlyChildrenOfObject(
+            parent: availableObjectsGameObject,
+            onlyPickupable: true,
+            onlyReceptacles: false,
+            tag: ""
+        );
+    }
+
+    [MenuItem("ExpRoom/Add all pickupable receptacle prefabs to AvailableContainers")]
+    static void AddPickupableReceptaclesToAvailableContainers() {
+        GameObject availableContainersGameObject = GameObject.Find("AvailableContainers");
+        AddAllPrefabsToAsOnlyChildrenOfObject(
+            parent: availableContainersGameObject,
+            onlyPickupable: true,
+            onlyReceptacles: true,
+            tag: "Container"
+        );
+    }
+
+    [MenuItem("ExpRoom/Interactively add all pickupable prefabs to AvailableObjects")]
+    static void Init() {
+        ExpRoomEditor window = (ExpRoomEditor) EditorWindow.GetWindow(typeof(ExpRoomEditor));
+        window.Show();
+        window.position = new Rect(20, 80, 400, 300);
+    }
+
+    // Disable menu items if not in ExpRoom
+    [MenuItem("ExpRoom/Add all pickupable prefabs to AvailableObjects", true)]
+    [MenuItem("ExpRoom/Add all pickupable receptacle prefabs to AvailableContainers", true)]
+    [MenuItem("ExpRoom/Interactively add all pickupable prefabs to AvailableObjects", true)]
+    static bool HideMenuIfNotInExpRoom() {
+        Debug.Log(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        return UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "FloorPlan_ExpRoom";
+    }
+
+    void OnGUI() {
+        GUILayout.Space(3);
+        int oldValue = GUI.skin.window.padding.bottom;
+        GUI.skin.window.padding.bottom = -20;
+        Rect windowRect = GUILayoutUtility.GetRect(1, 17);
+        windowRect.x += 4;
+        windowRect.width -= 7;
+        GUI.skin.window.padding.bottom = oldValue;
+
+        if (GUILayout.Button("Find all pickupable")) {
+            GameObject availableObjectsGameObject = GameObject.Find("AvailableObjects");
+            sopAndPrefabTuples = AddAllPrefabsToAsOnlyChildrenOfObject(
+                parent: availableObjectsGameObject,
+                onlyPickupable: true,
+                onlyReceptacles: false
+            );
+        }
+
+        if (sopAndPrefabTuples != null) {
+            if (sopAndPrefabTuples.Count == 0) {
+                GUILayout.Label("No pickupable objects found");
+            } else {
+                GUILayout.Label("The following prefabs were found:");
+                scroll = GUILayout.BeginScrollView(scroll);
+
+
+                foreach ((SimObjPhysics, string) sopAndPath in sopAndPrefabTuples) {
+                    SimObjPhysics sop = sopAndPath.Item1;
+                    string path = sopAndPath.Item2;
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label(path, GUILayout.Width(4 * position.width / 8));
+                    GUILayout.Label(sop.Type.ToString(), GUILayout.Width(3 * position.width / 8));
+                    if (GUILayout.Button("Select", GUILayout.Width(position.width / 8 - 2))) {
+                        Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(path);
+                    }
+                    GUILayout.EndHorizontal();
+                }
+                GUILayout.EndScrollView();
+            }
+        }
+    }
+
+    public static void FixBoundingBox(SimObjPhysics sop) {
+
+        Vector3 startPos = sop.transform.position;
+        Quaternion startRot = sop.transform.rotation;
+
+        sop.transform.position = new Vector3(0f, 0f, 0f);
+        sop.transform.rotation = Quaternion.identity;
+
+        sop.BoundingBox.transform.position = new Vector3();
+        sop.BoundingBox.transform.rotation = Quaternion.identity;
+        sop.BoundingBox.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        Physics.SyncTransforms();
+
+        Bounds objBounds = new Bounds(
+            new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity),
+            new Vector3(-float.PositiveInfinity, -float.PositiveInfinity, -float.PositiveInfinity)
+        );
+        foreach (Collider c in sop.GetComponentsInChildren<Collider>()) {
+            if (c.enabled && !c.isTrigger) {
+                objBounds.Encapsulate(c.bounds);
+            }
+        }
+
+        BoxCollider bbox = sop.BoundingBox.GetComponent<BoxCollider>();
+        bbox.center = objBounds.center;
+        bbox.size = 2 * objBounds.extents;
+
+        foreach (Transform child in sop.transform) {
+            child.position = child.position - bbox.center;
+        }
+        bbox.transform.position = Vector3.zero;
+        bbox.center = Vector3.zero;
+
+        sop.transform.position = startPos;
+        sop.transform.rotation = startRot;
+    }
+
+    public static string CreateSHA256Hash(string path) {
+        SHA256 hasher = SHA256.Create();
+
+        using (System.IO.FileStream fs = System.IO.File.OpenRead(path)) {
+            return string.Join("", hasher.ComputeHash(fs).Select(b => b.ToString("x2")).ToArray());
+        }
+    }
+
+    public static string[] GetAllPrefabs() {
+        return AssetDatabase.GetAllAssetPaths().Where(
+            s => s.Contains(".prefab")
+                && !s.Contains("Custom Project")
+                && !s.Contains("Assets/Resources/")
+                && !s.Contains("PhysicsTestPrefabs")
+                && !s.Contains("(Old)")
+             // && s.Contains("Assets/Physics/SimObjsPhysics") &&
+        ).ToArray();
+    }
+
+    public static List<(SimObjPhysics, string)> AddAllPrefabsToAsOnlyChildrenOfObject(
+        GameObject parent,
+        bool onlyPickupable,
+        bool onlyReceptacles,
+        string tag = ""
+    ) {
+        List <(SimObjPhysics, string)> sopAndPrefabTuples = new List <(SimObjPhysics, string)>();
+        string[] allPrefabs = GetAllPrefabs();
+        int numAdded = 0;
+        foreach (string prefab in allPrefabs) {
+            GameObject go = null;
+            try {
+                Debug.Log($"Checking {prefab}");
+                go = (GameObject) AssetDatabase.LoadMainAssetAtPath(prefab);
+                SimObjPhysics sop = go.GetComponent<SimObjPhysics>();
+
+                // if (sop != null && sop.Type.ToString() == "AlarmClock") {
+                if (sop != null) {    
+                    bool pickupable = sop.PrimaryProperty == SimObjPrimaryProperty.CanPickup;
+                    bool isReceptacle = Array.IndexOf(sop.SecondaryProperties, SimObjSecondaryProperty.Receptacle) > -1 && sop.ReceptacleTriggerBoxes != null;
+                    if (
+                        (pickupable || !onlyPickupable)
+                        && (isReceptacle || !onlyReceptacles)
+                    ) {
+                        sopAndPrefabTuples.Add((go.GetComponent<SimObjPhysics>(), prefab));
+                        numAdded += 1;
+                    }
+                }
+            } catch {
+                try {
+                    if (go) {
+                        DestroyImmediate(go);
+                    }
+                } catch {}
+                Debug.LogWarning($"Prefab {prefab} failed to load.");
+            }
+        }
+
+        sopAndPrefabTuples = sopAndPrefabTuples.OrderBy(
+            sopAndPath => (
+                sopAndPath.Item1.Type.ToString(),
+                int.Parse("0" + new string(
+                    sopAndPath.Item1.name
+                    .Split('/')
+                    .Last()
+                    .Where(c => char.IsDigit(c))
+                    .ToArray()))
+            )
+        ).ToList();
+        Debug.Log(
+            $"Found {allPrefabs.Length} total prefabs of which {sopAndPrefabTuples.Count} were SimObjPhysics satisfying"
+            + $"onlyPickupable=={onlyPickupable} and onlyReceptacles=={onlyReceptacles}."
+        );
+
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+
+
+        // Note that you cannot (unfortunately) combine the two below loops into
+        // a single loop as doing something like
+        // foreach (Transform child in parent.transform) {
+        //     DestroyImmediate(child.gameObject);
+        // }
+        // Will actually miss a large number of children because the way that looping through
+        // parent.transform works (deleting while iterating results in missing elements).
+        List<GameObject> toDestroy = new List<GameObject>();
+        foreach (Transform child in parent.transform) {
+            toDestroy.Add(child.gameObject);
+        }
+        foreach (GameObject child in toDestroy) {
+            Debug.Log($"Attempting to destroy {child.gameObject}");
+            DestroyImmediate(child);
+        }
+
+        if (tag != "") {
+            tag = $"_{tag}";
+        }
+        for (int i = 0; i < sopAndPrefabTuples.Count; i++) {
+            SimObjPhysics sop = sopAndPrefabTuples[i].Item1;
+            string path = sopAndPrefabTuples[i].Item2;
+            string hash = CreateSHA256Hash(path).Substring(0, 8);
+
+            GameObject go = (GameObject) PrefabUtility.InstantiatePrefab(sop.gameObject);
+            FixBoundingBox(go.GetComponent<SimObjPhysics>());
+
+            go.name = $"{sop.Type.ToString()}{tag}_{hash}";
+            go.SetActive(false);
+            go.transform.position = new Vector3(-5f, i * 0.05f, -5f);
+            go.transform.parent = parent.transform;
+
+            sopAndPrefabTuples[i] = (go.GetComponent<SimObjPhysics>(), path);
+        }
+
+        return sopAndPrefabTuples;
+    }
+
+
+}
---
+++ unity/Assets/Scripts/ExpRoom/PhysicsRemoteFPSAgentController_partial_ExpRoom.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Priority_Queue;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.ImageEffects;
+using UnityStandardAssets.Utility;
+using RandomExtensions;
+
+namespace UnityStandardAssets.Characters.FirstPerson {
+    public partial class PhysicsRemoteFPSAgentController : BaseFPSAgentController {
+        public static void emptyEnumerator(IEnumerator enumerator) {
+            while (enumerator.MoveNext()) {}
+        }
+
+        public void WhichAvailableObjectsFitInAvailableContainers() {
+            StartCoroutine(whichAvailableObjectsFitInAvailableContainers());
+        }
+
+        public IEnumerator whichAvailableObjectsFitInAvailableContainers() {
+            GameObject availableObjectsGameObject = GameObject.Find("AvailableObjects");
+            GameObject availableContainersGameObject = GameObject.Find("AvailableContainers");
+
+            Action<SimObjPhysics> activateSop = (sop) => {
+                sop.gameObject.SetActive(true);
+                sop.ObjectID = sop.name;
+                physicsSceneManager.AddToObjectsInScene(sop);
+            };
+
+            Action<SimObjPhysics> deactivateSop = (sop) => {
+                sop.gameObject.SetActive(false);
+                physicsSceneManager.ObjectIdToSimObjPhysics.Remove(sop.ObjectID);
+            };
+
+            Vector3 middleOfTable = new Vector3(-0.5f, 0.9f, 0.2f);
+            Vector3 rightOfTable = new Vector3(0.534f, 0.9f, 0.2f);
+
+            Dictionary<string, Dictionary<string, float>> objNameToCoverNames = new Dictionary<string, Dictionary<string, float>>();
+
+            foreach (Transform toCoverTransform in availableObjectsGameObject.transform) {
+                SimObjPhysics toCover = toCoverTransform.gameObject.GetComponent<SimObjPhysics>();
+                objNameToCoverNames[toCover.name] = new Dictionary<string, float>();
+
+                if (toCover.GetComponent<Break>()) {
+                    toCover.GetComponent<Break>().Unbreakable = true;
+                }
+
+                activateSop(toCover);
+
+                if (!PlaceObjectAtPoint(
+                    target: toCover,
+                    position: middleOfTable,
+                    rotation: null,
+                    forceKinematic: true
+                )) {
+                    Debug.Log($"{toCover.name} failed to place");
+                    deactivateSop(toCover);
+                    continue;
+                }
+
+                yield return null;
+
+                Vector3 toCoverPos = toCover.transform.position;
+
+                int numberFit = 0;
+                foreach (Transform coverTransform in availableContainersGameObject.transform) {
+                    SimObjPhysics cover = coverTransform.gameObject.GetComponent<SimObjPhysics>();
+                    if (cover.GetComponent<Break>()) {
+                        cover.GetComponent<Break>().Unbreakable = true;
+                    }
+
+                    cover.transform.rotation = Quaternion.Euler(180f, 0f, 0f);
+                    activateSop(cover);
+
+                    BoxCollider coverBbox = cover.BoundingBox.GetComponent<BoxCollider>();
+                    float maxScale = Mathf.Min(
+                        0.5f / coverBbox.size.y,
+                        0.7f / coverBbox.size.x,
+                        0.7f / coverBbox.size.z,
+                        1.75f
+                    );
+                    float minScale = Mathf.Min(0.5f, maxScale / 2.0f);
+
+                    float lastScale = 1.0f;
+                    Func<float, bool> tryScale = (scale) => {
+                        emptyEnumerator(scaleObject(
+                            scale: scale / lastScale,
+                            target: cover,
+                            scaleOverSeconds: 0f,
+                            skipActionFinished: true
+                        ));
+                        lastScale = scale;
+
+                        Physics.SyncTransforms();
+
+                        if (!PlaceObjectAtPoint(
+                            target: cover,
+                            position: rightOfTable,
+                            rotation: null,
+                            forceKinematic: true,
+                            includeErrorMessage: true
+                        )) {
+                            Debug.Log($"{cover.name} failed to place: {errorMessage}");
+                            deactivateSop(cover);
+                            return false;
+                        }
+
+                        float coverY = cover.transform.position.y;
+                        cover.transform.position = new Vector3(
+                            toCoverPos.x,
+                            coverY,
+                            toCoverPos.z
+                        );
+
+                        Collider coverCollidingWith = UtilityFunctions.firstColliderObjectCollidingWith(
+                            go: cover.gameObject
+                        );
+                        if (coverCollidingWith == null) {
+                            bool toCoverVisible = isSimObjVisible(m_Camera, toCover, 10f);
+                            if (!toCoverVisible) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    };
+
+                    if (!tryScale(maxScale)) {
+                        yield return null;
+                        deactivateSop(cover);
+                        continue;
+                    }
+
+                    for (int i = 0; i <= 5; i++) {
+                        float newScale = (minScale + maxScale) / 2.0f;
+                        if (tryScale(newScale)) {
+                            maxScale = newScale;
+                        } else {
+                            minScale = newScale;
+                        }
+                        yield return null;
+                    }
+                    Debug.Log($"{toCover.name} fits under {cover.name} at {maxScale} scale");
+                    objNameToCoverNames[toCover.name][cover.name] = maxScale;
+                    numberFit += 1;
+
+                    emptyEnumerator(scaleObject(
+                        scale: 1.0f / lastScale,
+                        target: cover,
+                        scaleOverSeconds: 0f,
+                        skipActionFinished: true
+                    ));
+
+                    deactivateSop(cover);
+                }
+
+                Debug.Log(
+                    Newtonsoft.Json.JsonConvert.SerializeObject(
+                        objNameToCoverNames,
+                        Newtonsoft.Json.Formatting.None,
+                        new Newtonsoft.Json.JsonSerializerSettings() {
+                            ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
+                            ContractResolver = new ShouldSerializeContractResolver()
+                        }
+                    )
+                );
+
+                deactivateSop(toCover);
+
+                break;
+            }
+
+            actionFinished(true);
+        }
+
+    }
+}
