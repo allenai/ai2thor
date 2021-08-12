2a07967f36ac3580e0cf5ae989ec1af31eb2c18f
winthos <winson.whan@gmail.com>
2021-08-11 20:41:58 -0700

    MOCK HMD input

---
+++ unity/Assets/IsThereAHeadMountedDisplayPluggedInOrDoWeHaveToDoThisOtherThing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class IsThereAHeadMountedDisplayPluggedInOrDoWeHaveToDoThisOtherThing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!XRSettings.isDeviceActive)
+        {
+            Debug.Log("Oh No, no Headset found so uhhh hang on, wait, wait...");
+        }
+
+        else if(XRSettings.isDeviceActive && XRSettings.loadedDeviceName == "Mock HMD" ||
+        (XRSettings.loadedDeviceName == "MockHMDDisplay"))
+        {
+            Debug.Log("Using Virtual Mock HMD");
+        }
+
+        else
+        {
+        Debug.Log("Active Headset found, Device Name is: " + XRSettings.loadedDeviceName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
