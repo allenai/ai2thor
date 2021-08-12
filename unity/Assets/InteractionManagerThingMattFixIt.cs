9ed5f886722d63a961e33d449a2e6c4cdff1df90
winthos <winson.whan@gmail.com>
2021-08-12 14:50:36 -0700

    matt fix it

---
+++ unity/Assets/InteractionManagerThingMattFixIt.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class InteractionManagerThingMattFixIt : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset actionAsset;
+    [SerializeField] XRRayInteractor leftHand;
+    [SerializeField] XRRayInteractor rightHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var leftHand = actionAsset.FindActionMap("XRI LeftHand").FindAction("Select");//maybe activate?
+        leftHand.Enable();
+        leftHand.performed += doTheThing;
+
+        var rightHand = actionAsset.FindActionMap("XRI RightHand").FindAction("Select");//maybe activate?
+        rightHand.Enable();
+        rightHand.performed += doTheThing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void doTheThing(InputAction.CallbackContext context)
+    {
+        RaycastHit hit;
+
+        if(leftHand.GetCurrentRaycastHit(out hit))
+        {
+            if(hit.transform.GetComponent<ThingToMove>())
+            {
+                ThingToMove ttm = hit.transform.GetComponent<ThingToMove>();
+                //ttm.thingToMove.transform.rotate..do something
+            }
+        }
+
+        if(rightHand.GetCurrentRaycastHit(out hit))
+        {
+            if(hit.transform.GetComponent<ThingToMove>())
+            {
+                ThingToMove ttm = hit.transform.GetComponent<ThingToMove>();
+                //do the thing
+            }
+        }
+    }
+}
---
+++ unity/Assets/ThingToMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingToMove : MonoBehaviour
+{
+    [SerializeField] GameObject thingToMove;
+    /// USE THESE FOR POSITION CHANGE
+    [SerializeField] Vector3 openPosition;
+    [SerializeField] Vector3 closePosition;
+    //USE THESE FOR ROTATION DONT WORRY ABOUT IT
+    [SerializeField] Vector3 closeRotation;
+    [SerializeField] Vector3 openRotation;
+    public bool isOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
