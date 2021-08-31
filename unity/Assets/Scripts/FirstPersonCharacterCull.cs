fdc1543b769e4f7c90b449377f8d4d53987d9958
Eric Kolve <erick@allenai.org>
2021-08-31 13:20:47 -0700

    updating type of renderers to hide to MeshRenderer; removing Tall/Bot/DroneRenderers

--- unity/Assets/Scripts/FirstPersonCharacterCull.cs
+++ unity/Assets/Scripts/FirstPersonCharacterCull.cs
@@ -16,13 +16,9 @@ public class FirstPersonCharacterCull : MonoBehaviour {
         }
     }
 
-    public Renderer[] RenderersToHide; // Mesh renderer that you want this script's camera to cull
+    public MeshRenderer[] RenderersToHide; // Mesh renderer that you want this script's camera to cull
     public BaseAgentComponent FPSController;
-    public MeshRenderer[] TallRenderers;
-    // references to renderers for when the Agent is in Bot mode
-    public MeshRenderer[] BotRenderers;
-    // references to renderers for when agent is in Drone mode
-    public MeshRenderer[] DroneRenderers;
+    
     public void SwitchRenderersToHide(GameObject visibilityCapsule) {
         List<MeshRenderer> renderers = new List<MeshRenderer>();
         foreach (var r in visibilityCapsule.GetComponentsInChildren<MeshRenderer>()) {
