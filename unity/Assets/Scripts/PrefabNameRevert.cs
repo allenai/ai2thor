067dda0c24b97c936d4785b3280bf6a1652cde72
winthos <winson.whan@gmail.com>
2021-09-09 11:56:06 -0700

    update set doors, add prefab name revert

---
+++ unity/Assets/Scripts/PrefabNameRevert.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+ 
+public class PrefabNameRevert
+{
+    [MenuItem("Prefab/RevertName")]
+    static void RevertPrefabNames()
+    {
+        RevertAllNames(Selection.GetFiltered(typeof(GameObject), SelectionMode.OnlyUserModifiable));
+    }
+ 
+    public static void RemoveNameModification(UnityEngine.Object aObj)
+    {
+        var mods = new List<PropertyModification>(PrefabUtility.GetPropertyModifications(aObj));
+        for (int i = 0; i < mods.Count; i++)
+        {
+            if (mods[i].propertyPath == "m_Name")
+                mods.RemoveAt(i);
+        }
+        PrefabUtility.SetPropertyModifications(aObj, mods.ToArray());
+    }
+    public static void RevertAllNames(Object[] aObjects)
+    {
+        var items = new List<Object>();
+        foreach (var item in aObjects)
+        {
+            var prefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(item);
+            if (prefab != null)
+            {
+                Undo.RecordObject(item, "Revert perfab name");
+                item.name = prefab.name;
+                items.Add(item);
+            }
+        }
+        Undo.FlushUndoRecordObjects();
+        foreach (var item in items)
+        {
+            RemoveNameModification(item);
+        }
+    }
+}
