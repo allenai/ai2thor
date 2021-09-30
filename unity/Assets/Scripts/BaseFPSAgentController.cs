afd5e47bafad076a3e2e60076441284b8ea81fe3
winthos <winson.whan@gmail.com>
2021-09-30 15:39:48 -0700

    update image synthesis error after SetObjectPoses

--- unity/Assets/Scripts/BaseFPSAgentController.cs
+++ unity/Assets/Scripts/BaseFPSAgentController.cs
@@ -3155,6 +3155,12 @@ namespace UnityStandardAssets.Characters.FirstPerson {
                 Physics.autoSimulation = autoSim;
             }
             physicsSceneManager.ResetObjectIdToSimObjPhysics();
+
+            //update image synthesis since scene has changed
+            if (this.imageSynthesis && this.imageSynthesis.enabled) {
+                this.imageSynthesis.OnSceneChange();
+            }
+
             actionFinished(success);
         }
 
