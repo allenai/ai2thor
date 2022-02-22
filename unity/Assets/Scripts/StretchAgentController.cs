546c50bfa7cfbcec7d5224527e48e6ccb7ed26c2
Eli VanderBilt <eliv@allenai.org>
2022-02-22 15:49:49 -0800

    Tweaked Secondary Camera position again

--- unity/Assets/Scripts/StretchAgentController.cs
+++ unity/Assets/Scripts/StretchAgentController.cs
@@ -51,7 +51,7 @@ namespace UnityStandardAssets.Characters.FirstPerson {
             // set secondary arm-camera
             Camera fp_camera_2 = m_CharacterController.transform.Find("SecondaryCamera").GetComponent<Camera>();
             fp_camera_2.gameObject.SetActive(true);
-            fp_camera_2.transform.localPosition = new Vector3(0.0353f, 0.5088f, -0.0499f);
+            fp_camera_2.transform.localPosition = new Vector3(0.0353f, 0.5088f, -0.076f);
             fp_camera_2.transform.localEulerAngles = new Vector3(45f, 90f, 0f);
             fp_camera_2.fieldOfView = 60f;
             agentManager.registerAsThirdPartyCamera(fp_camera_2);
