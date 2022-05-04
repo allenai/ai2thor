5f895075209ff17ab8f185d142149dcfbf14f0ec
lucaw <lucaw@allenai.org>
2022-05-03 18:42:49 -0700

    Removing unnecessary epsilon offsets.

--- unity/Assets/Scripts/LocobotFPSAgentController.cs
+++ unity/Assets/Scripts/LocobotFPSAgentController.cs
@@ -53,8 +53,8 @@ namespace UnityStandardAssets.Characters.FirstPerson {
             crouchingLocalCameraPosition = m_Camera.transform.localPosition + new Vector3(0, -0.2206f, 0);// smaller y offset if Bot
 
             // limit camera from looking too far down/up
-            this.maxDownwardLookAngle = 60f + 1e-6f; // Adding 1e-6f in case of floating point issues.
-            this.maxUpwardLookAngle = 30f + 1e-6f;
+            this.maxDownwardLookAngle = 60f;
+            this.maxUpwardLookAngle = 30f;
             // this.horizonAngles = new float[] { 30.0f, 0.0f, 330.0f };
         }
 
