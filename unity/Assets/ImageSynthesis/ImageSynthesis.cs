fba90e2decce8da7bfc23b71400dcb0f426cfa39
Rachel Artiss <rachel.artiss@caci.com>
2022-05-16 20:09:42 -0400

    Only use floor tops/walls approach with hole adjacent parts of floor (MCS-1330).

--- unity/Assets/ImageSynthesis/ImageSynthesis.cs
+++ unity/Assets/ImageSynthesis/ImageSynthesis.cs
@@ -305,8 +305,7 @@ public class ImageSynthesis : MonoBehaviour {
 	private string EnsureFloorTexturesAndHolesHaveIdenticalTags(string objTag, Renderer r) {
 		bool floor = objTag.Length > "floor".Length && objTag.Substring(0, "floor".Length) == "floor";
 		bool isAdjacentToHole = objTag.Length > "floorWall".Length && objTag.Substring(0, "floorWall".Length) == "floorWall";
-		bool hole = floor && r.transform.position.y == -MCSMain.GetFloorWallDepth();
-
+		bool hole = floor && r.transform.position.y == -MCSMain.GetFloorDepth();
 
 		objTag = floor ? "floor" + r.material.name + (hole ? "hole" : "") + (isAdjacentToHole ? "floorWall" : "") : objTag;
 
