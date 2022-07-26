ae80ccb11729a12d76da5ebebf43b564578137a1
lucaw <lucaw@allenai.org>
2022-07-26 15:03:10 -0700

    Removing agentControllerType initialization parameter.
    Co-authored-by: Winson Han <winson.whan@gmail.com>

--- ai2thor/tests/test_unity.py
+++ ai2thor/tests/test_unity.py
@@ -59,7 +59,6 @@ def build_controller(**args):
 
 _wsgi_controller = build_controller(server_class=WsgiServer)
 _fifo_controller = build_controller(server_class=FifoServer)
-_stochastic_controller = build_controller(agentControllerType="stochastic", agentMode="stochastic")
 
 
 def skip_reset(controller):
@@ -86,10 +85,6 @@ def wsgi_controller():
     return reset_controller(_wsgi_controller)
 
 
-@pytest.fixture
-def stochastic_controller():
-    return reset_controller(_stochastic_controller)
-
 
 @pytest.fixture
 def fifo_controller():
@@ -98,7 +93,6 @@ def fifo_controller():
 
 fifo_wsgi = [_fifo_controller, _wsgi_controller]
 fifo = [_fifo_controller]
-fifo_wsgi_stoch = [_fifo_controller, _wsgi_controller, _stochastic_controller]
 
 BASE_FP28_POSITION = dict(x=-1.5, z=-1.5, y=0.901,)
 BASE_FP28_LOCATION = dict(
@@ -114,12 +108,12 @@ def teleport_to_base_location(controller: Controller):
 
 
 def setup_function(function):
-    for c in fifo_wsgi_stoch:
+    for c in fifo_wsgi:
         reset_controller(c)
 
 
 def teardown_module(module):
-    for c in fifo_wsgi_stoch:
+    for c in fifo_wsgi:
         c.stop()
 
 
@@ -139,16 +133,16 @@ def assert_images_far(image1, image2, min_mean_pixel_diff=10):
     return np.mean(np.abs(image1 - image2).flatten()) >= min_mean_pixel_diff
 
 
-def test_stochastic_controller(stochastic_controller):
-    stochastic_controller.reset(TEST_SCENE)
-    assert stochastic_controller.last_event.metadata["lastActionSuccess"]
+def test_agent_controller_type_no_longer_accepted(fifo_controller):
+    with pytest.raises(ValueError):
+        build_controller(
+            server_class=FifoServer,
+            agentControllerType="physics",
+            agentMode="default",
+        )
 
-def test_stochastic_mismatch(fifo_controller):
-    try:
-        c = fifo_controller.reset(agentControllerType="stochastic", agentMode="default")
-    except RuntimeError as e:
-        error_message = str(e)
-    assert error_message and error_message.startswith("Invalid combination of agentControllerType=stochastic and agentMode=default")
+    with pytest.raises(Exception):
+        fifo_controller.reset(agentControllerType="physics", agentMode="default")
 
 
 # Issue #514 found that the thirdPartyCamera image code was causing multi-agents to end
@@ -340,7 +334,7 @@ def test_target_invocation_exception(controller):
     ], "errorMessage should not be empty when OpenObject(x > 1)."
 
 
-@pytest.mark.parametrize("controller", fifo_wsgi_stoch)
+@pytest.mark.parametrize("controller", fifo_wsgi)
 def test_lookup(controller):
 
     e = controller.step(dict(action="RotateLook", rotation=0, horizon=0))
--- unity/Assets/Scripts/AgentManager.cs
+++ unity/Assets/Scripts/AgentManager.cs
@@ -167,90 +167,35 @@ public class AgentManager : MonoBehaviour {
         //"locobot" agentMode can use either default or "stochastic" agentControllerType
         //"drone" agentMode can ONLY use "drone" agentControllerType, and NOTHING ELSE (for now?)
         if (action.agentMode.ToLower() == "default") {
-            if (action.agentControllerType.ToLower() != "physics" && action.agentControllerType.ToLower() != "stochastic") {
-                Debug.Log("default mode must use either physics or stochastic controller. Defaulting to physics");
-                action.agentControllerType = "";
-                SetUpPhysicsController();
-            }
-
-            // if not stochastic, default to physics controller
-            if (action.agentControllerType.ToLower() == "physics") {
-                // set up physics controller
-                SetUpPhysicsController();
-            }
-
-            // if stochastic, set up stochastic controller
-            else if (action.agentControllerType.ToLower() == "stochastic") {
-                // set up stochastic controller
-                primaryAgent.actionFinished(success: false, errorMessage: "Invalid combination of agentControllerType=stochastic and agentMode=default. In order to use agentControllerType=stochastic, agentMode must be set to stochastic");
-                return;
-            }
+            SetUpPhysicsController();
         } else if (action.agentMode.ToLower() == "locobot") {
-            // if not stochastic, default to stochastic
-            if (action.agentControllerType.ToLower() != "stochastic") {
-                Debug.Log("'bot' mode only fully supports the 'stochastic' controller type at the moment. Forcing agentControllerType to 'stochastic'");
-                action.agentControllerType = "stochastic";
-            }
             // LocobotController is a subclass of Stochastic which just the agentMode (VisibilityCapsule) changed
             SetUpLocobotController(action);
-
         } else if (action.agentMode.ToLower() == "drone") {
-            if (action.agentControllerType.ToLower() != "drone") {
-                Debug.Log("'drone' agentMode is only compatible with 'drone' agentControllerType, forcing agentControllerType to 'drone'");
-                action.agentControllerType = "drone";
-            }
             SetUpDroneController(action);
-        } else if (action.agentMode.ToLower() == "stretch") {
+        } else if (agentMode == "stretch" || agentMode == "arm") {
+            if (agentMode == "stretch") {
                 SetUpStretchController(action);
-
-                action.autoSimulation = false;
-                physicsSceneManager.MakeAllObjectsMoveable();
-
-                if (action.massThreshold.HasValue) {
-                    if (action.massThreshold.Value > 0.0) {
-                        SetUpMassThreshold(action.massThreshold.Value);
-                    } else {
-                        var error = "massThreshold must have nonzero value - invalid value: " + action.massThreshold.Value;
-                        Debug.Log(error);
-                        primaryAgent.actionFinished(false, error);
-                        return;
-                    }
-                }
-        } else if (action.agentMode.ToLower() == "arm") {
-
-            if (action.agentControllerType == "") {
-                action.agentControllerType = "mid-level";
-                Debug.Log("Defaulting to mid-level.");
-            }
-
-            if (action.agentControllerType.ToLower() != "low-level" && action.agentControllerType.ToLower() != "mid-level") {
-                var error = "'arm' mode must use either low-level or mid-level controller.";
-                Debug.Log(error);
-                primaryAgent.actionFinished(success: false, errorMessage: error);
-                return;
-            } else if (action.agentControllerType.ToLower() == "mid-level") {
-                // set up physics controller
+            } else if (agentMode == "arm") {
                 SetUpArmController(true);
-                // the arm should currently be used only with autoSimulation off
-                // as we manually control Physics during its movement
-                action.autoSimulation = false;
-                physicsSceneManager.MakeAllObjectsMoveable();
-
-                if (action.massThreshold.HasValue) {
-                    if (action.massThreshold.Value > 0.0) {
-                        SetUpMassThreshold(action.massThreshold.Value);
-                    } else {
-                        var error = "massThreshold must have nonzero value - invalid value: " + action.massThreshold.Value;
-                        Debug.Log(error);
-                        primaryAgent.actionFinished(success: false, errorMessage: error);
-                        return;
-                    }
-                }
-
             } else {
-                var error = "unsupported";
+                // Should not be possible but being very defensive.
+                throw new ArgumentException($"Invalid agentMode {action.agentMode}");
+            }
+        } else {
+            var error = $"Invalid agentMode {action.agentMode}";
+            Debug.Log(error);
+            primaryAgent.actionFinished(success: false, errorMessage: error);
+            return;
+        }
+
+        if (action.massThreshold.HasValue) {
+            if (action.massThreshold.Value > 0.0) {
+                SetUpMassThreshold(action.massThreshold.Value);
+            } else {
+                var error = $"massThreshold must have nonzero value - invalid value: {action.massThreshold.Value}";
                 Debug.Log(error);
-                primaryAgent.actionFinished(success: false, errorMessage: error);
+                primaryAgent.actionFinished(false, error);
                 return;
             }
         }
@@ -1908,7 +1853,6 @@ public class ServerAction {
     public float maxDistance;// used in target circle spawning function
     public float noise;
     public ControllerInitialization controllerInitialization = null;
-    public string agentControllerType = "";
     public string agentMode = "default"; // mode of Agent, valid values are "default" "locobot" "drone", note certain modes are only compatible with certain controller types
 
     public float agentRadius = 2.0f;
