c09d86e2296e3a638a3e19eb05db0b5c5aef2ee8
Yongsen Mao <yma50@sfu.ca>
2022-09-09 14:34:05 -0700

    add scene objects extraction

--- unity/Assets/Editor/ConvertGLTF.cs
+++ unity/Assets/Editor/ConvertGLTF.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityGLTF;
 using Newtonsoft.Json.Linq;
@@ -41,22 +42,64 @@ public class ConvertGLTF : MonoBehaviour
         
     }
 
-    // public static string DictToString<TKey, TValue> (this IDictionary<TKey, TValue> dictionary)
-    // {
-    //     return "{" + string.Join(",", dictionary.Select(kv => kv.Key + "=" + kv.Value).ToArray()) + "}";
-    // }
+    public static void ExportObject(GameObject game_object, string output_dir)
+    {
+        MethodInfo export_method = typeof(GLTFExportMenu).GetMethod("Export", BindingFlags.NonPublic | BindingFlags.Static);
+        Transform[] transforms = new Transform[] {game_object.transform};
+        export_method.Invoke(null, new object[] {transforms, true, game_object.name});
+
+        Transform[] children_nodes;
+        children_nodes = game_object.GetComponentsInChildren<Transform>(true);
+        JObject asset_metadata = new JObject();
+        foreach (Transform child in children_nodes) {
+            GameObject child_node = child.gameObject;
+            Transform parent_transform = child.parent;
+            int parent_id = -1;
+            if (parent_transform != null)
+                parent_id = parent_transform.gameObject.GetInstanceID();
+            ComponentMetadata component = new ComponentMetadata();
+            component.name = child_node.name;
+            component.layer = child_node.layer;
+            component.tag = child_node.tag;
+            component.isActive = child_node.activeSelf;
+            // Vector3 openRot = coo.openPositions[i];
+            // Vector3 closeRot = coo.closedPositions[i];
+            component.localPosition = child.localPosition;
+            component.localRotation = child.localRotation;
+            component.localScale = child.localScale;
+            component.parentId = parent_id.ToString();
+            string component_json = JsonUtility.ToJson(component);
+            asset_metadata[child_node.GetInstanceID().ToString()] = JObject.Parse(component_json);
+
+            MonoBehaviour[] scripts;
+            scripts = child_node.GetComponents<MonoBehaviour>();
+            JObject script_metadata = new JObject();
+            foreach (MonoBehaviour script in scripts) {
+                string script_name = script.GetType().Name;
+                if (script.GetType() == typeof(SimObjPhysics)) {
+                    SimObjPhysics sim = (SimObjPhysics) script;
+                    sim.Init();
+                }
+                string script_json = JsonUtility.ToJson(script);
+                script_metadata[script_name] = JObject.Parse(script_json);
+            }
+            if (script_metadata.Count > 0) {
+                asset_metadata[child_node.GetInstanceID().ToString()][typeof(MonoBehaviour).Name] = script_metadata;
+            }
+        }
+        File.WriteAllText(output_dir + "/" + game_object.name + ".json", asset_metadata.ToString());
+    }
 
 #if UNITY_EDITOR
 
     [MenuItem("ConvertGLTF/Convert prefab models to glb format")]
-    public static void ConvertToGLB() {
-        // Find all assets labelled with 'architecture' :
-        string[] prefab_guids = AssetDatabase.FindAssets("t:Prefab", new string[] {"Assets/Physics/SimObjsPhysics"});
-
+    public static void ConvertObjectToGLB() {
         var path = EditorUtility.SaveFolderPanel("glTF Export Path", GLTFSceneExporter.SaveFolderPath, "");
-
         GLTFSceneExporter.SaveFolderPath = path;
         Debug.Log(GLTFSceneExporter.SaveFolderPath);
+
+        // Find all assets labelled with 'architecture' :
+        string[] prefab_guids = AssetDatabase.FindAssets("t:Prefab", new string[] {"Assets/Physics/SimObjsPhysics"});
 
         foreach (string prefab_guid in prefab_guids)
         {
@@ -64,52 +107,34 @@ public class ConvertGLTF : MonoBehaviour
             GameObject game_object = AssetDatabase.LoadAssetAtPath<GameObject>(prefab_path);
             Debug.Log(game_object.name);
 
-            // export to glb file
-            // MethodInfo export_method = typeof(GLTFExportMenu).GetMethod("Export", BindingFlags.NonPublic | BindingFlags.Static);
-            // Transform[] transforms = new Transform[] {game_object.transform};
-            // export_method.Invoke(null, new object[] {transforms, true, game_object.name});
+            ExportObject(game_object, path);
+        }
+    }
 
-            // export to metadata json file
-            Transform[] children_objects;
-            children_objects = game_object.GetComponentsInChildren<Transform>(true);
-            JObject asset_metadata = new JObject();
-            foreach (Transform child in children_objects) {
-                GameObject child_object = child.gameObject;
-                Transform parent_transform = child.parent;
-                int parent_id = -1;
-                if (parent_transform != null)
-                    parent_id = parent_transform.gameObject.GetInstanceID();
-                ComponentMetadata component = new ComponentMetadata();
-                component.name = child_object.name;
-                component.layer = child_object.layer;
-                component.tag = child_object.tag;
-                component.isActive = child_object.activeSelf;
-                // Vector3 openRot = coo.openPositions[i];
-                // Vector3 closeRot = coo.closedPositions[i];
-                component.localPosition = child.localPosition;
-                component.localRotation = child.localRotation;
-                component.localScale = child.localScale;
-                component.parentId = parent_id.ToString();
-                string component_json = JsonUtility.ToJson(component);
-                asset_metadata[child_object.GetInstanceID().ToString()] = JObject.Parse(component_json);
+    [MenuItem("ConvertGLTF/Convert scene objects to glb format")]
+    public static void ConvertSceneToGLB() {
+        var path = EditorUtility.SaveFolderPanel("glTF Export Path", GLTFSceneExporter.SaveFolderPath, "");
 
-                MonoBehaviour[] scripts;
-                scripts = child_object.GetComponents<MonoBehaviour>();
-                JObject script_metadata = new JObject();
-                foreach (MonoBehaviour script in scripts) {
-                    string script_name = script.GetType().Name;
-                    if (script.GetType() == typeof(SimObjPhysics)) {
-                        SimObjPhysics sim = (SimObjPhysics) script;
-                        sim.Init();
+        string[] scene_guids = AssetDatabase.FindAssets("t:Scene", new string[] {"Assets/Scenes"});
+        foreach (string scene_guid in scene_guids) {
+            var scene_path = AssetDatabase.GUIDToAssetPath(scene_guid);
+            Debug.Log(scene_path);
+            
+            Scene scene = UnityEditor.SceneManagement.EditorSceneManager.OpenScene(scene_path);
+            string output_dir = path + "/" + scene.name;
+            GLTFSceneExporter.SaveFolderPath = output_dir;
+            Debug.Log(GLTFSceneExporter.SaveFolderPath);
+            GameObject[] roots = scene.GetRootGameObjects();
+            foreach (GameObject root in roots) {
+                if (root.name == "Objects" || root.name == "Structure") {
+                    foreach (Transform child in root.transform) {
+                        Debug.Log(child.name);
+                        GameObject game_object = child.gameObject;
+                        ExportObject(game_object, output_dir);
                     }
-                    string script_json = JsonUtility.ToJson(script);
-                    script_metadata[script_name] = JObject.Parse(script_json);
-                }
-                if (script_metadata.Count > 0) {
-                    asset_metadata[child_object.GetInstanceID().ToString()][typeof(MonoBehaviour).Name] = script_metadata;
                 }
             }
-            File.WriteAllText(path + "/" + game_object.name + ".json", asset_metadata.ToString());
+            break;
         }
     }
 
