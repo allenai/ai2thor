9d4cb4382049e4f66c145d2e3cf7a3fc0e313808
Matt Deitke <28768645+mattdeitke@users.noreply.github.com>
2022-09-16 10:09:19 -0700

    obj action

--- unity/Assets/Scripts/DebugInputField.cs
+++ unity/Assets/Scripts/DebugInputField.cs
@@ -3777,6 +3777,32 @@ namespace UnityStandardAssets.Characters.FirstPerson {
                         break;
                     }
 
+                case "obj": {
+                        // AssetDatabase.Refresh();
+                        var OBJECTS_BASE_PATH = "/Resources/objects/";
+
+                        path = Application.dataPath + OBJECTS_BASE_PATH + "main.json";
+
+                        if (splitcommand.Length == 2) {
+                            // uses ./debug/{splitcommand[1]}[.json]
+                            file = splitcommand[1].Trim();
+                            if (!file.EndsWith(".json")) {
+                                file += ".json";
+                            }
+                            path = Application.dataPath + OBJECTS_BASE_PATH + file;
+                        }
+
+                        var jsonStr = System.IO.File.ReadAllText(path);
+                        Debug.Log($"jjson: {jsonStr}");
+
+                        JObject obj = JObject.Parse(jsonStr);
+
+                        obj["action"] = "CreateObjectPrefab";
+                        CurrentActiveController().ProcessControlCommand(new DynamicServerAction(obj));
+
+                        break;
+                    }
+
                 case "chp": {
 
                         Dictionary<string, object> action = new Dictionary<string, object>();
