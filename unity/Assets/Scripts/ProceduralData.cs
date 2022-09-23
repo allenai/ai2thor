c1e887eb79a518f65b3a3a7fe67752193f8c8a3b
Matt Deitke <28768645+mattdeitke@users.noreply.github.com>
2022-09-22 21:47:52 -0700

    update types

--- unity/Assets/Scripts/ProceduralData.cs
+++ unity/Assets/Scripts/ProceduralData.cs
@@ -140,6 +140,31 @@ namespace Thor.Procedural.Data {
 
     [Serializable]
     [MessagePackObject(keyAsPropertyName: true)]
+    public class SerializableCollider {
+        public Vector3[] vertices;
+        public int[] triangles;
+    }
+
+    [Serializable]
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class PhysicalProperties {
+        public float mass = 1;
+        public float drag = 0;
+        public float angularDrag = 0.05f;
+        public bool useGravity = true;
+        public bool isKinematic = false;
+    }
+
+    [Serializable]
+    [MessagePackObject(keyAsPropertyName: true)]
+    public class ObjectAnnotations {
+        public string objectType = "Undefined";
+        public string primaryProperty = "Undefined";
+        public string[]? secondaryProperties = null;
+    }
+
+    [Serializable]
+    [MessagePackObject(keyAsPropertyName: true)]
     public class Ceiling {
         public string id { get; set; }
         public List<Vector3> polygon { get; set; }
