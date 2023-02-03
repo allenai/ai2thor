b04e76fd861b4ababa1b7d7e51a17c2cb65e3fa7
Zichen "Charles" Zhang <52727818+zcczhang@users.noreply.github.com>
2023-02-02 20:40:23 -0800

    uncomment local position & euler

--- unity/Assets/Scripts/StretchAgentController.cs
+++ unity/Assets/Scripts/StretchAgentController.cs
@@ -61,8 +61,8 @@ namespace UnityStandardAssets.Characters.FirstPerson {
             // set secondary arm-camera
             Camera fp_camera_3 = m_CharacterController.transform.Find("GripperCamera").GetComponent<Camera>();
             fp_camera_3.gameObject.SetActive(true);
-//            fp_camera_3.transform.localPosition = new Vector3(0f, -0.09862678f, 0f);
-//            fp_camera_3.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            fp_camera_3.transform.localPosition = new Vector3(0f, -0.09862678f, 0f);
+            fp_camera_3.transform.localEulerAngles = new Vector3(0f, 0f, 0f);
 //            fp_camera_3.fieldOfView = 90f;
             fp_camera_3.fieldOfView = 75f;
             agentManager.registerAsThirdPartyCamera(fp_camera_3);
