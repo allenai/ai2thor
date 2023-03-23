9169bf06b7d7c60725a0e36fec99b61a4b2c5466
Alvaro Herrasti Gonzalez <alvaroh@ip-192-168-68-120.us-west-2.compute.internal>
2023-03-22 21:43:38 -0600

    Fixes stretch agent for cloudrendering

--- unity/Assets/Scripts/AgentManager.cs
+++ unity/Assets/Scripts/AgentManager.cs
@@ -369,7 +369,9 @@ public class AgentManager : MonoBehaviour {
 
     public void registerAsThirdPartyCamera(Camera camera) {
         this.thirdPartyCameras.Add(camera);
-        // camera.gameObject.AddComponent(typeof(ImageSynthesis));
+        #if PLATFORM_CLOUD_RENDERING
+        camera.targetTexture = createRenderTexture(this.primaryAgent.m_Camera.pixelWidth, this.primaryAgent.m_Camera.targetTexture.height);
+        #endif
     }
 
     // If fov is <= min or > max, return defaultVal, else return fov
