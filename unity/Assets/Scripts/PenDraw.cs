6eb58aa62ddd4277c980bb1c82bdcaa88f2faaf8
Winson Han <winson.whan@gmail.com>
2023-07-26 10:57:01 -0700

    fix pen/dirt decal layermask issue

--- unity/Assets/Scripts/PenDraw.cs
+++ unity/Assets/Scripts/PenDraw.cs
@@ -26,13 +26,20 @@ public class PenDraw : MonoBehaviour
         if (other.CompareTag("DecalSpawnPlane") && shouldSpawn) {
             RaycastHit hit;
             //check if we hit the spawn plane below the pencil
-            if (Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("SimObjVisible", "Default"))) {
-                Debug.DrawRay(hit.point, Vector3.up * 10, Color.red);
-
+            if (Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("Default"))) {
+                
+                //Debug.DrawRay(hit.point, Vector3.up * 10, Color.red);
+                
+                //check if we hit another pen mark, if so don't place anything because its too close
                 if (hit.collider.tag == "Pen") {
                     return;
-                } else {
-                    Object.Instantiate(penDecal, hit.point, Quaternion.Euler(-90, 0, 0));
+                } 
+                
+                //ok so if its not a pen mark, that means we hit a dirt mark which means we can spawn on the table
+                else {
+                    if (Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("SimObjVisible"))) {
+                        Object.Instantiate(penDecal, hit.point, Quaternion.Euler(-90, 0, 0));
+                    }
                 }
             }
         }
