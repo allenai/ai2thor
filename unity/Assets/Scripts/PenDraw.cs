7de4b989864f55a647f034b77f8003b227d81f9d
winthos <winson.whan@gmail.com>
2023-07-25 17:24:11 -0700

    Update PenDraw.cs

--- unity/Assets/Scripts/PenDraw.cs
+++ unity/Assets/Scripts/PenDraw.cs
@@ -21,46 +21,20 @@ public class PenDraw : MonoBehaviour
         
     }
 
-    // void OnCollisionStay (Collision collisionInfo) {
-    //     foreach (ContactPoint contact in collisionInfo.contacts)
-    //     {
-    //         Debug.DrawRay(contact.point, contact.normal * 10, Color.white);
-    //     }
-    // }
+    void OnTriggerStay(Collider other) {
 
-    void OnTriggerStay (Collider other) {
-
-        if(other.CompareTag("DecalSpawnPlane") && shouldSpawn) {
-            
+        if (other.CompareTag("DecalSpawnPlane") && shouldSpawn) {
             RaycastHit hit;
             //check if we hit the spawn plane below the pencil
-            if(Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("SimObjVisible", "Default")))
-            {
+            if (Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("SimObjVisible", "Default"))) {
                 Debug.DrawRay(hit.point, Vector3.up * 10, Color.red);
 
-                if(hit.collider.tag == "Dirt") {
+                if (hit.collider.tag == "Dirt") {
                     return;
-                }
-
-                else {
+                } else {
                     Object.Instantiate(penDecal, hit.point, Quaternion.Euler(-90, 0, 0));
-
                 }
-
-                // RaycastHit dirtHit;
-                // //ok cool now check if there is already another decal there, if there is, then we don't spawn so it doesn't overlap forever and crash
-                // if(Physics.Raycast(raycastOrigin.transform.position, raycastOrigin.transform.forward, out dirtHit, Mathf.Infinity, LayerMask.GetMask("Dirt"), QueryTriggerInteraction.Collide))
-                // {
-                //     if(dirtHit.collider.tag == "Dirt") {
-                //         return;
-                //     }
-                // }
-
-                // else {
-                //     Object.Instantiate(penDecal, hit.point, Quaternion.Euler(-90, 0, 0));
-                // }
             }
         }
-
     }
 }
