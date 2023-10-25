a85e2dcaa39cb36367c2d121fe55fb6f3d139400
Eli VanderBilt <40370237+elimvb@users.noreply.github.com>
2023-10-25 14:22:56 -0700

    Changed hotkey for texture reload to simple "s"

--- unity/Assets/Scripts/ProceduralAssetEditor.cs
+++ unity/Assets/Scripts/ProceduralAssetEditor.cs
@@ -249,7 +249,7 @@ namespace Thor.Procedural {
             }
         }
 
-         [UnityEditor.MenuItem("Procedural/Reload Procedural Asset Textures %e")]
+         [UnityEditor.MenuItem("Procedural/Reload Procedural Asset Textures _s")]
         public static void ReloadTextures() {
              var procAssets = GameObject.FindObjectsOfType(typeof(RuntimePrefab))  as RuntimePrefab[];
              foreach (var asset in procAssets) { 
