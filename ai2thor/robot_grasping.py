2cddc259e020b13103e712f5a788bfbb92719ffc
Yejin Kim <yejink@ip-192-168-255-98.us-west-2.compute.internal>
2023-11-09 13:56:50 -0800

    add camera pose estimator

--- ai2thor/robot_grasping.py
+++ ai2thor/robot_grasping.py
@@ -30,13 +30,14 @@ from scipy.spatial.transform import Rotation as R
 import math
 import torch
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
+
 
 ## CONSTANTS TRANSFORMATION MATRIX
-T_ARM_FROM_BASE_188 = np.array([[   -0.99906,    -0.04335,  -0.0003883,   -0.047584],
-                                [  -0.020913,     0.48978,     -0.8716,   -0.053311],
-                                [   0.037974,    -0.87077,    -0.49022,      1.4462],
-                                [          0,           0,           0,           1]])
-
+T_ARM_FROM_BASE_188 = np.array([[   -0.99736,  -0.0073698,   -0.072253,   -0.025143],
+       [  -0.048995,     0.80264,     0.59445,    -0.76168],
+       [   0.053612,     0.59642,    -0.80088,      1.4598],
+       [          0,           0,           0,           1]])
 
 T_ARM_FROM_BASE_205 = np.array([[   -0.99652,   -0.080247,   -0.022519,   -0.055535],
        [  -0.023487,     0.52961,    -0.84792,   -0.053421],
@@ -53,6 +54,60 @@ STRETCH_INTR = {'coeffs': [0.0, 0.0, 0.0, 0.0, 0.0], 'fx': 911.8329467773438, 'f
 ARM_INTR = {'coeffs': [-0.05686680227518082, 0.06842068582773209, -0.0004524677060544491, 0.0006787769380025566, -0.022475285455584526], 'fx': 640.1092529296875, 'fy': 639.4522094726562, 'height': 720, 'ppx': 652.3712158203125, 'ppy': 368.69549560546875, 'width': 1280, 'depth_scale': 0.0010000000474974513}
 
 
+## POSE ESTIMATION
+def my_estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
+    '''
+    This will estimate the rvec and tvec for each of the marker corners detected by:
+       corners, ids, rejectedImgPoints = detector.detectMarkers(image)
+    corners - is an array of detected corners for each detected marker in the image
+    marker_size - is the size of the detected markers
+    mtx - is the camera matrix
+    distortion - is the camera distortion matrix
+    RETURN list of rvecs, tvecs, and trash (so that it corresponds to the old estimatePoseSingleMarkers())
+    '''
+    marker_points = np.array([[-marker_size / 2, marker_size / 2, 0],
+                              [marker_size / 2, marker_size / 2, 0],
+                              [marker_size / 2, -marker_size / 2, 0],
+                              [-marker_size / 2, -marker_size / 2, 0]], dtype=np.float32)
+    trash = []
+    rvecs = []
+    tvecs = []
+    
+    for c in corners:
+        nada, R, t = cv2.solvePnP(marker_points, c, mtx, distortion, False, cv2.SOLVEPNP_IPPE_SQUARE)
+        rvecs.append(R)
+        tvecs.append(t)
+        trash.append(nada)
+    return rvecs, tvecs, trash
+
+def inverse_homogeneous_matrix(matrix):
+    """
+    Compute the inverse of a 4x4 homogeneous transformation matrix.
+
+    Args:
+    matrix (numpy.ndarray): A 4x4 homogeneous transformation matrix.
+
+    Returns:
+    numpy.ndarray: The inverse of the input matrix.
+    """
+    if matrix.shape != (4, 4):
+        raise ValueError("Input matrix must be a 4x4 matrix.")
+
+    rotation_matrix = matrix[0:3, 0:3]
+    translation_vector = matrix[0:3, 3]
+
+    inverse_rotation = np.transpose(rotation_matrix)
+    inverse_translation = -np.dot(inverse_rotation, translation_vector)
+
+    inverse_matrix = np.identity(4)
+    inverse_matrix[0:3, 0:3] = inverse_rotation
+    inverse_matrix[0:3, 3] = inverse_translation
+
+    return inverse_matrix
+
+
+
+
 class BaseObjectDetector():
     def __init__(self, camera_source="arm205"):
         self.camera_source = camera_source
@@ -60,6 +115,79 @@ class BaseObjectDetector():
 
     #def predict_instance_segmentation(self, rgb):
     #    raise NotImplementedError
+
+    def initialize_camera_pose(self, controller):
+        camera_sources = controller.camera_sources
+        controller.update_camera_sources(camera_sources=["arm","stretch"])
+        controller.step("Stow")
+        controller.step("RotateHead")
+        controller.step({"action":[
+                    {"action": "MoveArmExtension", "args": {"move_scalar": 0.3}},
+                    {"action": "MoveArmBase", "args": {"move_scalar": 0.65}},
+        ]})
+
+        arm_image = c.last_event.third_party_camera_frames[0]
+        stretch_image = c.last_event.third_party_camera_frames[1]
+
+        # aruco marker detector
+        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+        parameters =  cv2.aruco.DetectorParameters()
+        detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+        length_of_marker_mm = 23.5
+
+        # stretch image
+        gray_stretch_image = cv2.cvtColor(stretch_image, cv2.COLOR_BGR2GRAY)
+        stretch_aruco_corners, stretch_aruco_ids, aruco_rejected_image_points = detector.detectMarkers(gray_stretch_image)
+
+        # arm image
+        gray_arm_image = cv2.cvtColor(arm_image, cv2.COLOR_BGR2GRAY)
+        arm_aruco_corners, arm_aruco_ids, aruco_rejected_image_points = detector.detectMarkers(gray_arm_image)
+
+        # pose (stretch)
+        stretch_camera_matrix = np.array([[STRETCH_INTR["fx"], 0, STRETCH_INTR["ppx"]], [0, STRETCH_INTR["fy"], STRETCH_INTR["ppy"]], [0, 0, 1]])
+        stretch_distortion_coefficients = np.array(STRETCH_INTR["coeffs"])
+        rvecs, tvecs, unknown_variable = my_estimatePoseSingleMarkers(stretch_aruco_corners,
+                                                                                length_of_marker_mm,
+                                                                                stretch_camera_matrix,
+                                                                                stretch_distortion_coefficients)
+
+        stretch_aruco_rotation = rvecs[0]
+        stretch_aruco_position = tvecs[0]/1000.0 # Convert ArUco position estimate to be in meters.
+
+        P_aruco_from_stretch = np.identity(4)
+        P_aruco_from_stretch[:3,:3] = cv2.Rodrigues(stretch_aruco_rotation)[0]
+        P_aruco_from_stretch[0:3,3] = stretch_aruco_position.T
+
+        # pose (arm)
+        arm_camera_matrix = np.array([[ARM_INTR["fx"], 0, ARM_INTR["ppx"]], [0, ARM_INTR["fy"], ARM_INTR["ppy"]], [0, 0, 1]])
+        arm_distortion_coefficients = np.array(ARM_INTR["coeffs"])
+        rvecs, tvecs, unknown_variable = my_estimatePoseSingleMarkers(arm_aruco_corners,
+                                                                                length_of_marker_mm,
+                                                                                arm_camera_matrix,
+                                                                                arm_distortion_coefficients)
+
+        arm_aruco_rotation = rvecs[0]
+        arm_aruco_position = tvecs[0]/1000.0 # Convert ArUco position estimate to be in meters.
+        
+        P_aruco_from_arm = np.identity(4)
+        P_aruco_from_arm[:3,:3] = cv2.Rodrigues(arm_aruco_rotation)[0]
+        P_aruco_from_arm[0:3,3] = arm_aruco_position.T
+
+
+        # transformation
+        P_arm_from_aruco = inverse_homogeneous_matrix(P_aruco_from_arm)
+        T_arm_from_stretch =  P_aruco_from_stretch @ P_arm_from_aruco
+        r = R.from_quat([0.616, 0.616, -0.346, 0.345]) # camera_color_optical_frame
+        T_stretch_from_base = np.identity(4)
+        T_stretch_from_base[0:3, 0:3] = r.as_matrix()
+        T_stretch_from_base[0:3, 3] = np.array([-0.017, -0.038, 1.294])
+
+        T_arm_from_base =  T_stretch_from_base @ T_arm_from_stretch
+        self.CameraPose = T_arm_from_base
+
+        # reset camera
+        controller.update_camera_sources(camera_sources=camera_sources)
+        
 
     def update_camera_info(self, camera_source):
         ## TODO: read from config
@@ -89,7 +217,7 @@ class BaseObjectDetector():
 
     def get_target_object_pose(self, rgb, depth, mask, visualize=False):
         if mask is None:
-            exit()
+            return None
 
         rgb = np.array(rgb.copy())
         rgbim = open3d.geometry.Image(rgb.astype(np.uint8))
@@ -102,25 +230,30 @@ class BaseObjectDetector():
         rgbd = open3d.geometry.RGBDImage.create_from_color_and_depth(rgbim, depthim, convert_rgb_to_intensity=False)
         pcd = open3d.geometry.PointCloud.create_from_rgbd_image(rgbd, self.intrinsic)
         print("pcd pointcloud numbers: ", len(pcd.points))
-        pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+        #pcd = pcd.voxel_down_sample(voxel_size=0.02)
+        #print("pcd pointcloud numbers after down sample: ", len(pcd.points))
+        pcd, ind = pcd.remove_radius_outlier(nb_points=20, radius=0.02)
+        #pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=0.5)
         print("pcd pointcloud numbers after outlier removal: ", len(pcd.points))
+
 
         center = pcd.get_center()
         bbox = pcd.get_oriented_bounding_box()
         
         ##TODO: if len(pcd.points) is zero, there is a problem with a depth image)
-        Randt=np.concatenate((bbox.R, np.expand_dims(center, axis=1)),axis=1) # pitfall: arrays need to be passed as a tuple
-        lastrow=np.expand_dims(np.array([0,0,0,1]),axis=0)
-        objectPoseCamera = np.concatenate((Randt,lastrow)) 
+        #Randt=np.concatenate((bbox.R, np.expand_dims(center, axis=1)),axis=1) # pitfall: arrays need to be passed as a tuple
+        #lastrow=np.expand_dims(np.array([0,0,0,1]),axis=0)
+        #objectPoseCamera = np.concatenate((Randt,lastrow)) 
 
         Randt=np.concatenate((bbox.R, np.expand_dims(bbox.center, axis=1)),axis=1) # pitfall: arrays need to be passed as a tuple
-        objectPoseCamera1 = np.concatenate((Randt,lastrow)) 
-        print("Center vs bbox center:", (self.CameraPose @ objectPoseCamera)[:3,3], (self.CameraPose @ objectPoseCamera1)[:3,3])
+        lastrow=np.expand_dims(np.array([0,0,0,1]),axis=0)
+        objectPoseCamera = np.concatenate((Randt,lastrow)) 
+        #print("Center vs bbox center:", (self.CameraPose @ objectPoseCamera)[:3,3], (self.CameraPose @ objectPoseCamera1)[:3,3])
 
         ObjectPose = self.CameraPose @ objectPoseCamera
-        ObjectPose1 = self.CameraPose @ objectPoseCamera1
-        ObjectPose1[0,3] = ObjectPose[0,3]
-        ObjectPose1[1,3] = ObjectPose[1,3]
+        #ObjectPose1 = self.CameraPose @ objectPoseCamera1
+        #ObjectPose1[0,3] = ObjectPose[0,3]
+        #ObjectPose1[1,3] = ObjectPose[1,3]
 
 
         if visualize:
@@ -131,7 +264,7 @@ class BaseObjectDetector():
             plt.show()
 
             obj_center = open3d.geometry.TriangleMesh.create_sphere(radius=0.015) #create a small sphere to represent point
-            obj_center.translate(ObjectPose1[:3,3])
+            obj_center.translate(ObjectPose[:3,3])
             obj_center.paint_uniform_color([255,0,0])
 
             coord = open3d.geometry.TriangleMesh().create_coordinate_frame()
@@ -148,7 +281,7 @@ class BaseObjectDetector():
             vis.run()
             vis.destroy_window() # this kills Jupyter Notebook kernel for some reason
 
-        return ObjectPose1 #self.CameraPose @ objectPoseCamera
+        return ObjectPose #self.CameraPose @ objectPoseCamera
 
 
 
@@ -310,7 +443,7 @@ class DoorKnobDetector(OwlVitSegAnyObjectDetector):
 
     def get_target_object_pointcloud(self, rgb, depth, mask):
         if mask is None:
-            exit()
+            return None 
 
         _rgb = np.array(rgb.copy())
         rgbim = open3d.geometry.Image(_rgb.astype(np.uint8))
@@ -327,7 +460,9 @@ class DoorKnobDetector(OwlVitSegAnyObjectDetector):
     def get_target_object_pose(self, rgb, depth, mask, distance_m=0.215, visualize=False): # -0.205 
         pcd = self.get_target_object_pointcloud(rgb, depth, mask)
         ##TODO: if len(pcd.points) is zero, there is a problem with a depth image)
-
+        if pcd is None:
+            return [None, None]
+        
         self.center = pcd.get_center()
         bbox = pcd.get_oriented_bounding_box()
 
@@ -654,13 +789,13 @@ class VIDAGraspPlanner(GraspPlanner):
         super().__init__()
 
         ## 188 constants
-        self.wrist_yaw_from_base = 0.003 # 25 #-0.020 # -0.025 # FIXED - should be.
-        self.arm_offset = 0.155 # 0.140
+        self.wrist_yaw_from_base = 0.0025 # 25 #-0.020 # -0.025 # FIXED - should be.
+        self.arm_offset = 0.20 # end of arm to wrist offset
         self.lift_base_offset = 0.192 # base to lift
         self.lift_wrist_offset = 0.028
 
     def plan_lift_extenion(self, object_position, curr_lift_position):
-        lift_object_offset = -0.025 # to grasp a little lower than the estimated cetner
+        lift_object_offset = -0.03 # to grasp a little lower than the estimated cetner
         return (object_position[2]+lift_object_offset) + 0.168 - (curr_lift_position-0.21) - 0.41 #meters
 
     def get_wrist_position(self, last_event):
@@ -733,10 +868,13 @@ class VIDAGraspPlanner(GraspPlanner):
                 #x_delta, y_delta, z_delta = (object_position - wrist_position)
                 y_delta = -1*abs(y_delta + new_arm_position) 
                 wrist_offset = np.degrees(np.arctan2(-x_delta, -y_delta)) # arctan2(y,x)
-                if wrist_offset >= 75.0: #max Wrist Rotation
-                    trajectory = []
-                    isReachable=False
-                    continue 
+                if wrist_offset > 0:
+                    #wrist_offset *= 0.95
+                    if wrist_offset >= 75.0: #max Wrist Rotation
+                        trajectory = []
+                        isReachable=False
+                        print("wrist offset requires : ", wrist_offset)
+                        continue 
                 trajectory.append({"action": "WristTo", "args": {"move_to":  wrist_offset}})
 
                 # TODO: check z_delta before  - will it hit the object?
@@ -748,7 +886,8 @@ class VIDAGraspPlanner(GraspPlanner):
                 # lift - will it hit the object? most likely the arm is higher than the object....
                 trajectory.append({"action": "MoveArmBase", "args": {"move_scalar": self.plan_lift_extenion(object_position, last_event.metadata["arm"]["lift_m"])}})
                 isReachable=True 
-    
+            else:
+                print("nnew arm position is too long:. ", new_arm_position)
             
         return isReachable, {"action": trajectory}
 
