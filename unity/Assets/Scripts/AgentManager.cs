16ee9366608df237faa51c02cd27c8480c0b7d99
AlvaroHG <alvaroherrasti@gmail.com>
2024-11-20 20:25:47 -0800

    Prefab update

--- unity/Assets/Scripts/AgentManager.cs
+++ unity/Assets/Scripts/AgentManager.cs
@@ -190,7 +190,7 @@ public class AgentManager : MonoBehaviour, ActionInvokable {
 
         //Why is Rendering manager not in cloudrendering?
 
-        gameObject.AddComponent(typeof(RenderingManager));
+        // gameObject.AddComponent(typeof(RenderingManager));
 
         // Camera camera = this.primaryAgent.gameObject.GetComponentInChildren<Camera>();
         // camera.targetTexture = createRenderTexture(Screen.width, Screen.height);
