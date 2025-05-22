04defd29f0cb5fd4971654131d788e6b0cc5d79e
AlvaroHG <alvaroherrasti@gmail.com>
2025-05-22 15:26:29 -0700

    Missing scripts from refactor

---
+++ unity/Assets/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace Thor.Attributes {
+   
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}   
---
+++ unity/Assets/Scripts/Attributes/ReadOnlyDrawer.cs
@@ -0,0 +1,22 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using Thor.Attributes;
+
+namespace Thor.Drawers
+{
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+    
+}
+
+#endif
