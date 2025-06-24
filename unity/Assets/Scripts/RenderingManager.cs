c410946c2f79165c2e8cb9e77d3b2295715ad0c6
AlvaroHG <alvaroherrasti@gmail.com>
2025-06-23 19:59:56 -0700

    Check if null

--- unity/Assets/Scripts/RenderingManager.cs
+++ unity/Assets/Scripts/RenderingManager.cs
@@ -110,12 +110,14 @@ public class RenderingManager : MonoBehaviour {
         var mainCamera = GetComponent<Camera>();
         Debug.Log($"===== OnCameraChange Rendering manager null {this == null} manager {this} camera null {mainCamera == null} cam {mainCamera}");
 
-        Debug.Log($"===== OnCameraChange multipass for {string.Join(", ", this.activePasses.Values.Select(x => x.GetName()))}");
-
-        
-        foreach (var pass in this.activePasses) {
-            Debug.Log($"--- OnCameraChange {pass.Key} is null {pass.Value == null} pass {pass.Value}");
-            pass.Value.OnCameraChange(mainCamera);
+        if (this.activePasses != null) {
+            foreach (var pass in this.activePasses) {
+                Debug.Log($"--- OnCameraChange {pass.Key} is null {pass.Value == null} pass {pass.Value}");
+                pass.Value.OnCameraChange(mainCamera);
+            }
+        }
+        else {
+            Debug.Log($"===== this.activePasses is null {this.activePasses == null} passe {this.activePasses}");
         }
     }
 
