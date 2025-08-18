c2313e186e01b848382c16875c082d32af31bf55
AlvaroHG <alvaroherrasti@gmail.com>
2025-08-18 15:45:31 -0600

    Adds OnPreRender OnPostRender callbacks and before waiting to render for beaker debugging

--- unity/Assets/Scripts/AgentManager.cs
+++ unity/Assets/Scripts/AgentManager.cs
@@ -1831,6 +1831,7 @@ public class AgentManager : MonoBehaviour, ActionInvokable {
                 }
             }
 
+            Debug.Log("AgentManager.cs: WaitForEndOfFrame call waiting for unity to render.");
             yield return new WaitForEndOfFrame();
 
             frameCounter += 1;
